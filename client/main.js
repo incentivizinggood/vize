279cfb1f5510123797de8f604454a9e1e1ca6c33
Thomas M <tmcdonald1234@yahoo.com>
2017-10-04 14:55:05 -0500

    Restructured project

--- client/main.js
+++ client/main.js
@@ -1,5 +1,34 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
 import './main.html';
+//import { RouterModule, Routes } from '@angular/router';
 
-angular.module('main-site', [
+var webApp = angular.module('main-site', [
     angularMeteor,
+    'ngRoute',
 ]);
+/*
+webApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            //controller: 'HomeController',
+            templateUrl: 'imports/components/home/home.html'
+        })
+        .when('/about', {
+            templateUrl: 'imports/components/about/about.html'
+        })
+        .when('/companies',{
+            templateUrl: 'imports/components/companies/companies.html'
+        })
+        .when('/contact-us',{
+            templateUrl: 'imports/components/contact-us/contact.html'
+        })
+        .when('/login', {
+            templateUrl: 'imports/components/login/login.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+*/
---
+++ imports/startup/client/routes.js
@@ -0,0 +1,13 @@
+//import {FlowRouter} from FlowRouter;
+
+FlowRouter.route('/',{
+
+}).route('/about',{
+
+}).route('/companies',{
+
+}).route('/contact-us',{
+
+}).route('/login',{
+
+});
