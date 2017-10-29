3dc33b76add1c77f1b2132a9020b7ba48fe3634d
Thomas M <tmcdonald1234@yahoo.com>
2017-10-29 09:58:31 -0500

    Added contact-view and routing. Does not work

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { Router, Route } from 'react-router';
 import './main.html';
 
-Meteor.startup(() => {
+import Contact from '../imports/ui/contact-view.jsx';
 
+Meteor.startup(() => {
+    render(
+        <Contact />,
+        document.getElementById('view-render')
+    );
 });
+/*
+render(
+    <Router>
+        <Route path='/contact-us' component={Contact}/>
+    </Router>,
+    document.getElementById('view-render')
+);
+
+*/
