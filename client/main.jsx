e606fa5536e64f3c587808667fcceb5cdb4f0850
Thomas M <tmcdonald1234@yahoo.com>
2017-10-29 10:08:51 -0500

    Fixed contact-view filename. Routing still does not work

--- client/main.jsx
+++ client/main.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Router, Route } from 'react-router';
-import './main.html';
 
 import Contact from '../imports/ui/contact-view.jsx';
 
 Meteor.startup(() => {
     render(
-        <Contact />,
+        <Router>
+            <Route path='/contact-us' component={Contact}/>
+        </Router>,
         document.getElementById('view-render')
     );
-});
-/*
-render(
-    <Router>
-        <Route path='/contact-us' component={Contact}/>
-    </Router>,
-    document.getElementById('view-render')
-);
 
-*/
+});
+
+
+
