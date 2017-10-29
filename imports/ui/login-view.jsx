647dd1d13a7c8bd664a1c2e3b22017393b2d1563
Thomas M <tmcdonald1234@yahoo.com>
2017-10-29 12:21:50 -0500

    Added login screen

--- imports/ui/login-view.jsx
+++ imports/ui/login-view.jsx
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 
 export default class Login extends Component {
     render() {
-        return (<h1>Login</h1>);
+        return (
+            <div className='container'>
+                <form className="form-signin">
+                    <h2 className="form-signin-heading">Please sign in</h2>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                    <div className="checkbox">
+                        <label>
+                            <input type="checkbox" value="remember-me"/>
+                            Remember Me
+                        </label>
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
+            </div>
+        );
     }
 }
