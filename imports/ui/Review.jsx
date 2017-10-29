2a30ba703692278a48715d7e4c021169a41b6015
Tarun Thummala <tarun@investwithvi.com>
2017-10-29 11:51:13 -0500

    did some more stuff

--- imports/ui/Review.jsx
+++ imports/ui/Review.jsx
@@ -1,2 +1,25 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+ 
+import { Reviews } from '../api/reviews.js';
+
+export default class Reviews extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Reviews</h1>
+                <span className="text">{this.props.reviews.text}</span>
+            </div>
+        );
+    }
+}
+
+Reviews.propTypes = {
+  reviews: PropTypes.array.isRequired,
+};
+ 
+export default createContainer(() => {
+  return {
+    reviews: Reviews.find({}).fetch(),
+  };
+}, App);
