590d85eb6489eda12c5fe02338ac5088d51ed7b8
Thomas M <tmcdonald1234@yahoo.com>
2017-10-29 10:28:51 -0500

    Added home

--- client/main.jsx
+++ client/main.jsx
@@ -3,17 +3,21 @@ import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Router, Route } from 'react-router';
 
+import './main.html';
+
+import Home from '../imports/ui/home-view.jsx';
 import Contact from '../imports/ui/contact-view.jsx';
 
 Meteor.startup(() => {
     render(
         <Router>
-            <Route path='/contact-us' component={Contact}/>
+            <Route path='/' component={Home}/>
+            <Route path='/contact-us' component={ Contact }/>
         </Router>,
         document.getElementById('view-render')
     );
-
 });
+
 
 
 
