dc15f68db3c305803ec35a979cf90d67e3fb81eb
Urel <perfect123@Urels-MacBook-Pro.local>
2018-01-09 11:20:53 -0600

    Star ratings

---
+++ imports/ui/StarRating.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <html>
+            <head>
+                <title>About</title>
+                <link type="text/css" rel="stylesheet" href="./main.css"/>
+            </head>
+            <body class = "about">
+
+
+            </body>
+            </html>
+
+        );
+    }
+}
