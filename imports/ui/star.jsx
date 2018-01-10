9272898b4d71f1b9460a13b56a5c8c746178d34c
Urel <perfect123@Urels-MacBook-Pro.local>
2018-01-10 11:11:11 -0600

    Saving starting

--- imports/ui/star.jsx
+++ imports/ui/star.jsx
@@ -6,8 +6,8 @@ export default class Star extends React.Component {
     constructor() {
         super();
         this.state = {
-            rating: 6,
-            rating2: 5
+            rating: 1,
+            rating2: 1
         };
     }
 
@@ -18,29 +18,59 @@ export default class Star extends React.Component {
         this.setState({rating2: nextValue});
     }
 
+    // Handling the submit from forms
+    handleSubmit(event){
+        alert('A name was submitted: ' + this.state.rating);
+        event.preventDefault();
+    }
+
     render() {
         const {rating} = this.state;
         const {rating2} = this.state;
 
         return (
-            <div>
-                <br/>
-                <h2>Rating from state: {rating}</h2>
-                <StarRatingComponent
-                    name="rate1"
-                    starCount={10}
-                    value={rating}
-                    onStarClick={this.onStarClick.bind(this)}
-                />
-                <br/>
-                <h2>Rating from state: {rating2}</h2>
-                <StarRatingComponent
-                    name="rate2"
-                    starCount={10}
-                    value={rating2}
-                    onStarClick={this.onStarClick2.bind(this)}
-                />
-            </div>
+            <html>
+            <head>
+                <title>Forms</title>
+                <link type="text/css" rel="stylesheet" href="./main.css"/>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"/>
+            </head>
+            <body>
+
+            <form className="new-task" onSubmit={this.handleSubmit}>
+                <fieldset>
+                    <br/>
+                    <br/>
+                    <legend>Share your experiences here!!</legend>
+                    <input type="text" placeholder="title:"/><br/>
+                    <textarea type="text" placeholder="Enter text here .." rows="6" cols="50" name="comment" form="usrform"></textarea><br/>
+                    Wages:
+                    <input type="number" ref="wagesInput" name="enteredWages" min="1" max= "100"/><br/>
+
+                    <div>
+                            <h2>Health and Safety: {rating}</h2>
+                            <StarRatingComponent
+                                name="rate1"
+                                starCount={5}
+                                value={rating}
+                                onStarClick={this.onStarClick.bind(this)}
+                            />
+                            <br/>
+                            <h2>Recommended: {rating2}</h2>
+                            <StarRatingComponent
+                                name="rate2"
+                                starCount={5}
+                                value={rating2}
+                                onStarClick={this.onStarClick2.bind(this)}
+                            />
+                    </div>
+                    <br/>
+                    <input type="submit" value="Submit" />
+                </fieldset>
+            </form>
+            </body>
+            </html>
+
         );
     }
 }
