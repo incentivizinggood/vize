b733b2304840e66ddc00870c3be9b8638bf0ab9f
joshuatshaffer <joshuatshaffer@gmail.com>
2018-01-20 14:36:32 -0600

    Fixed incorrect html syntax.

--- imports/ui/pages/login.jsx
+++ imports/ui/pages/login.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import { withHistory, Link } from 'react-router-dom'
 import { createContainer } from "meteor/react-meteor-data";
 
 export default class LoginPage extends Component {
@@ -29,86 +28,41 @@ export default class LoginPage extends Component {
     render() {
         const error = this.state.error;
         return (
-            <html>
-                <head>
-                    <title>Login</title>
-                </head>
-                <body>
-                    <section class="sectionContainer">
-                        <div>
-                            <h2>Credentials</h2>
-                            <form method="post" onSubmit="" action="index.html">
-                                <p>
-                                    <input
-                                        type="text"
-                                        name="login"
-                                        value=""
-                                        placeholder="Username or email"
-                                    />
-                                </p>
-                                <p>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        value=""
-                                        placeholder="Password"
-                                    />
-                                </p>
+            <div className="page login">
+                <section className="sectionContainer">
+                    <div>
+                        <h2>Credentials</h2>
+                        <form method="post" onSubmit="" action="index.html">
+                            <p>
+                                <input
+                                    type="text"
+                                    name="login"
+                                    value=""
+                                    placeholder="Username or email"
+                                />
+                            </p>
+                            <p>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value=""
+                                    placeholder="Password"
+                                />
+                            </p>
 
-                                <p class="remember_me">
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            name="remember_me"
-                                            id="remember_me"
-                                        />
-                                    </label>
-                                </p>
-                            </form>
-                        </div>
-                    </section>
-                </body>
-            </html>
+                            <p className="remember_me">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="remember_me"
+                                        id="remember_me"
+                                    />
+                                </label>
+                            </p>
+                        </form>
+                    </div>
+                </section>
+            </div>
         );
     }
 }
-
-/*
-import React, {Component} from 'react';
-
-export default class Login extends Component {
-    render() {
-        return (
-            <html>
-            <head>
-                <link rel="stylesheet" type="text/css" href="../imports.css"/>
-            </head>
-                <body>
-                <div className='container'>
-                    <form className="form-signin">
-                        <h2 className="form-signin-heading">Please sign in</h2>
-                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
-                        <label htmlFor="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                        <div className="checkbox">
-                            <label>
-                                <input type="checkbox" value="remember-me"/>
-                                Remember Me
-                            </label>
-                        </div>
-                        <button className="btn btn-lg btn-primary btn-block" type="submit-button">Sign in</button>
-                        <button type="signup-button" href={""}>Sign up</button>
-                    </form>
-                    <p> Forgot your password? <a href={"google.com"}>Click Here!</a></p>
-                </div>
-                </body>
-            </html>
-        );
-    }
-}
-
-
-Remove this tag
-Don't have an account? Register <Link to="/signup">here</Link>
-*/
--- imports/ui/pages/register.jsx
+++ imports/ui/pages/register.jsx
@@ -29,109 +29,77 @@ export default class RegisterPage extends Component {
             }
         );
     }
-    ///*
+
     render() {
         const error = this.state.error;
         return (
-            <div className="modal show">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="text-center">Sign up</h1>
+            <div className="page register">
+                <div className="modal show">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="text-center">Sign up</h1>
+                            </div>
+                            <div className="modal-body">
+                                {error.length > 0 ? (
+                                    <div className="alert alert-danger fade in">
+                                        {error}
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                                <form
+                                    id="login-form"
+                                    className="form col-md-12 center-block"
+                                    onSubmit={this.handleSubmit}
+                                >
+                                    <div className="form-group">
+                                        <input
+                                            type="text"
+                                            id="signup-name"
+                                            className="form-control input-lg"
+                                            placeholder="name"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <input
+                                            type="email"
+                                            id="signup-email"
+                                            className="form-control input-lg"
+                                            placeholder="email"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <input
+                                            type="password"
+                                            id="signup-password"
+                                            className="form-control input-lg"
+                                            placeholder="password"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <input
+                                            type="submit"
+                                            id="login-button"
+                                            className="btn btn-lg btn-primary btn-block"
+                                            value="Sign Up"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <p className="text-center">
+                                            Already have an account? Login
+                                        </p>
+                                    </div>
+                                </form>
+                            </div>
+                            <div
+                                className="modal-footer"
+                                style={{ borderTop: 0 }}
+                            />
                         </div>
-                        <div className="modal-body">
-                            {error.length > 0 ? (
-                                <div className="alert alert-danger fade in">
-                                    {error}
-                                </div>
-                            ) : (
-                                ""
-                            )}
-                            <form
-                                id="login-form"
-                                className="form col-md-12 center-block"
-                                onSubmit={this.handleSubmit}
-                            >
-                                <div className="form-group">
-                                    <input
-                                        type="text"
-                                        id="signup-name"
-                                        className="form-control input-lg"
-                                        placeholder="name"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="email"
-                                        id="signup-email"
-                                        className="form-control input-lg"
-                                        placeholder="email"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="password"
-                                        id="signup-password"
-                                        className="form-control input-lg"
-                                        placeholder="password"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="submit"
-                                        id="login-button"
-                                        className="btn btn-lg btn-primary btn-block"
-                                        value="Sign Up"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <p className="text-center">
-                                        Already have an account? Login
-                                    </p>
-                                </div>
-                            </form>
-                        </div>
-                        <div
-                            className="modal-footer"
-                            style={{ borderTop: 0 }}
-                        />
                     </div>
                 </div>
             </div>
         );
     }
-
-    //    */
-    // <Link to="/login">here</Link> I took out link from the register and the loginview
-    /*
-    render() {
-        return (
-            <html>
-            <head>
-                <link rel="stylesheet" type="text/css" href="../imports.css"/>
-            </head>
-            <body>
-            <div className='container'>
-                <form className="form-signin">
-                    <h2 className="form-signin-heading">Please sign in</h2>
-                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
-                    <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                    <div className="checkbox">
-                        <label>
-                            <input type="checkbox" value="remember-me"/>
-                            Remember Me
-                        </label>
-                    </div>
-                    <button className="btn btn-lg btn-primary btn-block" type="submit-button">Sign in</button>
-                    <button type="signup-button" href={""}>Sign up</button>
-                </form>
-                <p> Forgot your password? <a href={"google.com"}>Click Here!</a></p>
-            </div>
-            </body>
-            </html>
-        );
-    }
-    */
 }
