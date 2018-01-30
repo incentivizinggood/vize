ec786e1795a68d00bc5105f2bba883f85eada339
joshuatshaffer <joshuatshaffer@gmail.com>
2018-01-30 08:25:40 -0600

    Added not found (404) page.

--- imports/startup/client/router.jsx
+++ imports/startup/client/router.jsx
@@ -7,6 +7,7 @@ import AboutPage from "../../ui/pages/about.jsx";
 import LoginPage from "../../ui/pages/login.jsx";
 import CompaniesPage from "../../ui/pages/companies.jsx";
 import RegisterPage from "../../ui/pages/register.jsx";
+import NotFoundPage from "../../ui/pages/not-found.jsx";
 
 // Reduces boiler plate for simple pages.
 function routeSimplePage(path, pageType) {
@@ -26,3 +27,13 @@ routeSimplePage("/companies", CompaniesPage);
 routeSimplePage("/contact-us", ContactUsPage);
 routeSimplePage("/login", LoginPage);
 routeSimplePage("/register", RegisterPage);
+
+// Add a 404 page to handle unknown paths.
+FlowRouter.notFound = {
+    action() {
+        ReactDOM.render(
+            <NotFoundPage />,
+            document.getElementById("view-render")
+        );
+    }
+};
---
+++ imports/ui/pages/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class NotFoundPage extends React.Component {
+    render() {
+        return (
+            <div className="page not-found">
+                <h1>404</h1>
+                <h2>That page could not be found.</h2>
+            </div>
+        );
+    }
+}
