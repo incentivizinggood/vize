aeb2bf947063ba9f8b9dbfbe3948d5de804816e4
joshuatshaffer <joshuatshaffer@gmail.com>
2018-03-18 08:40:35 -0500

    Changed login page to use controlled components.

--- imports/ui/pages/login.jsx
+++ imports/ui/pages/login.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createContainer } from "meteor/react-meteor-data";
 
 /* The page where users can login to the app.
  */
@@ -7,59 +6,40 @@ export default class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null,
-            success: false
+            error: Meteor.userId() === null ? null : "Already loged in",
+            success: false,
+            username: "",
+            password: ""
         };
 
         // These bindings are necessary to make `this` work in callbacks.
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.loginCallback = this.loginCallback.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    loginCallback(error) {
-        if (error) {
-            this.setState({
-                error: error.reason,
-                success: false
-            });
-        } else {
-            this.setState({
-                error: null,
-                success: true
-            });
-        }
+    handleInputChange(event) {
+        const target = event.target;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     handleSubmit(event) {
         event.preventDefault(); // Prevent the default behavior for this event.
-        let name = this.nameInput.value;
-        let password = this.passwordInput.value;
-        Meteor.loginWithPassword(name, password, this.loginCallback);
-    }
-
-    renderErrorMessage() {
-        if (this.state.error) {
-            return <div>{this.state.error}</div>;
-        } else {
-            return null;
-        }
-    }
-
-    renderLoginForm() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username or email"
-                    ref={x => (this.nameInput = x)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    ref={x => (this.passwordInput = x)}
-                />
-                <input type="submit" value="Login" />
-            </form>
+        let loginCallback = error => {
+            this.setState({
+                error: error ? error.reason : null,
+                success: !error
+            });
+        };
+        Meteor.loginWithPassword(
+            this.state.username,
+            this.state.password,
+            loginCallback
         );
     }
 
@@ -69,8 +49,27 @@ export default class LoginPage extends React.Component {
         }
         return (
             <div className="page login">
-                {this.renderErrorMessage()}
-                {this.renderLoginForm()}
+                {this.state.error ? <div>{this.state.error}</div> : null}
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        autoFocus
+                        required
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                    />
+                    <input type="submit" value="Login" />
+                </form>
             </div>
         );
     }
