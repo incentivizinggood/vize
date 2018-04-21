fbb1dec98d85e2a6373d3e8b9d21b58613522ab3
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-04-21 14:08:15 -0500

    OH MY GOODNESS I THINK IT MIGHT JUST WORK. Basically, I just got a React-wrapped-Blaze 'Hello World', and I'm super pumped. Will see what else I can do with it.

--- imports/ui/pages/create-company-profile.jsx
+++ imports/ui/pages/create-company-profile.jsx
@@ -1,16 +1,27 @@
+//"standard" imports
 import React from "react";
 import { Mongo } from "meteor/mongo";
 import { Companies } from "../../api/data/companies.js";
-// import SimpleSchema from "simpl-schema";
-// SimpleSchema.extendOptions(['autoform']);
+
+//so we can use Blaze packages
+import { Template } from 'meteor/templating';
+import Blaze from 'meteor/gadicc:blaze-react-component';
+
+//autoform business
+import SimpleSchema from "simpl-schema";
+SimpleSchema.extendOptions(['autoform']);
+
+//now the interesting part...
+import "./ccp_blaze_form.html";
+Template.ccp_blaze_form.helpers({
+	companyProfiles: Companies,
+});
 
 export default class CompanyCreateProfileForm extends React.Component {
 	render() {
 		return (
-			<div className="page create-company-profile">
-				<h1>
-					Hello world
-				</h1>
+			<div className="page CompanyCreateProfileForm">
+				<Blaze template="ccp_blaze_form" />
 			</div>
 		);
 	}
