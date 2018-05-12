8f4831fd86bcb85ce3a94b4fbe9960158c191675
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-05-12 17:17:21 -0500

    Migrated WriteReviewForm to use a reactive dict instead of a reactive var, in preparation for sharing props with the Blaze template via helpers, also verified that it can indeed receive the props. This is exciting.

--- imports/ui/pages/write-review.jsx
+++ imports/ui/pages/write-review.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { Template } from "meteor/templating"; // Used to set up the autoform
 import Blaze from "meteor/gadicc:blaze-react-component"; // used to insert Blaze templates into React components
 import ErrorWidget from "../error-widget.jsx"; // used to display errors thrown by methods
-import { ReactiveVar } from "meteor/reactive-var"; // used to hold global state because...you can't "pass props" to Blaze templates
+import { ReactiveDict } from "meteor/reactive-dict"; // used to hold global state because...you can't "pass props" to Blaze templates
 import { AutoForm } from "meteor/aldeed:autoform";
-let formError = new ReactiveVar("good");
+
 
 //Specific stuff second
 import { Reviews } from "../../api/data/reviews.js";
@@ -18,36 +18,40 @@ import "./wr_blaze_form.html";
 import "../afInputStarRating.html";
 import "../afInputStarRating.js";
 
+let wr_form_state = new ReactiveDict();
+wr_form_state.set("formError", "good");
+
 Template.wr_blaze_form.helpers({
 	reviews: Reviews,
 	ErrorWidget: function() {
 		return ErrorWidget;
 	},
 	hasError: function() {
-		return formError.get() !== "good";
+		return wr_form_state.get("formError") !== "good";
 	},
 	error: function() {
-		return formError.get();
+		return wr_form_state.get("formError");
 	},
 	resetFormError: function() { //called when reset button is clicked
-		formError.set("good");
+		wr_form_state.set("formError", "good");
 	},
 });
 
 AutoForm.addHooks("wr_blaze_form", {
 	onSuccess: function(formType, result) { // If your method returns something, it will show up in "result"
 		console.log("SUCCESS: We did a thing in a " + formType + " form: " + result);
-		formError.set("good");
+		wr_form_state.set("formError", "good");
 	},
 	onError: function(formType, error) { // "error" contains whatever error object was thrown
 		console.log("ERROR: We did a thing in a " + formType + " form: " + error);
-		formError.set(error.toString());
+		wr_form_state.set("formError", error.toString());
 	},
 });
 
 export default class WriteReviewForm extends React.Component {
 	constructor (props) {
 		super(props);
+		//console.log("GOT AN ID: " + this.props.companyId);
 	}
 	render() {
 		return (
