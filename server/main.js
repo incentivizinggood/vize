49ebefdca8c6606925e44c05b23b384f2e7a4689
Urel <perfect123@Urels-MacBook-Pro.local>
2018-05-16 11:28:12 -0500

    Message box

--- server/main.js
+++ server/main.js
@@ -11,58 +11,11 @@ import "../imports/api/data/users.js";
 import "../imports/api/data/methods.js";
 
 Meteor.startup(() => {
-<<<<<<< HEAD
     // code to run on server at startup
     // process.env.MAIL_URL = "***REMOVED***";
     // process.env.MAIL_URL = "***REMOVED***";
 //    process.env.MAIL_URL = "***REMOVED***";
     process.env.MAIL_URL = "***REMOVED***";
 
-    // Meteor.call(
-    //     "sendEmail",
-    //     "urelperfect@gmail.com",
-    //     "postmaster@incentivizinggood.com",
-    //     "You sent an email!",
-    //     "Hello,\n\n\t New email before email .\n\nSincerely,\n\n\tUrel Djiogan\n\n",
-    //     (err, res) => {
-    //         if (err) {
-    //             console.log("--- BEGIN error:");
-    //             console.log(err);
-    //             console.log("--- END error")
-    //         } else {
-    //             console.log("--- BEGIN success:");
-    //             console.log(res);
-    //             console.log("--- END success")
-    //         }
-    //     }
-    // );
-});
-=======
-	// code to run on server at startup
-	// process.env.MAIL_URL = "***REMOVED***";
-	process.env.MAIL_URL =  "***REMOVED***";
-
-	/* Imports for server-side startup go here. */
-	console.log("SERVER: before call2");
-	Meteor.call(
-		"sendEmail",
-		"perfectpud@yahoo.com",
-		"postmaster@mg.incentivizinggood.com",
-		"You received an Email!",
-		"Hey Urel,\n\n\tDid a bid of cleanup and wanted to check things again. Hope you're doing well.\n\nSincerely,\n\n\tJoshua Higginbotham\n\n",
-		(err,res) => {
-			if (err) {
-				console.log("--- BEGIN error:");
-				console.log(err);
-				console.log("--- END error");
-			} else {
-				console.log("--- BEGIN success:");
-				console.log(res);
-				console.log("--- END success");
-			}
-		}
-	);
-	console.log("SERVER: after call");
 
 });
->>>>>>> 716f72695707486f3b8760a22548b650570401aa
