3b9b543b599d9e95b0a0ca91c2366d6d09422dea
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-05-17 15:55:48 -0500

    Undid changes to foremployers.jsx

--- imports/ui/pages/foremployers.jsx
+++ imports/ui/pages/foremployers.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../ui/pages/header.jsx";
-import Footer from "../../ui/pages/footer.jsx";
+import Footer from "../../ui/pages/footer.jsx"; 
 
 
 /* A page Foremployers  */
@@ -12,25 +12,25 @@ export default class ForEmployers extends React.Component
                          <div>
                          <Header />
                          <div className="employer">
-                         <div className="banner-info">
+                         <div className="banner-info">		
 				             <div className="banner-text">
                                <div  id="top" className="callbacks_container">
 								 <ul className="rslides" id="slider3">
 									         <li>
 									        <div className="banner-text-info">
-									        <h1>Attract more workers to optimize for workforce</h1>
+									        <h1>Attract more workers to optimize for workforce</h1>				
 											   <form className="example" method="POST" action="#">
 			                                   <input type="text" placeholder="Search for a company..." name="search"/>
 			                                   <button type="submit">SEARCH</button>
 			                                   </form>
-
-
-										</div>
+			                              
+				
+										</div>	
 									</li>
-
+									
 								</ul>
 							</div>
-				             </div>
+				             </div>	
 		                 </div>
 		                 </div>
                          <div className="welcome welpad">
@@ -38,7 +38,7 @@ export default class ForEmployers extends React.Component
 						      <div className="col-md-12 ">
 						         <h1>Give yourself the best chance</h1>
 						      </div>
-
+						     
 						      <div className="col-md-12 ">
 						         <h2>JobAdvisor helps you discover which employers exist and compare them side by side. Apply for jobs, let them know you're interested, or simply follow the ones you like to add them to your short-list. </h2>
 						      </div>
@@ -47,27 +47,27 @@ export default class ForEmployers extends React.Component
 						            <div className="titlestar">
 						               <a href="#" className="button out-butt-dark">TAKE A LOOK</a>
 						            </div>
-
+						          
 						         </div>
 						      </div>
 						      <div className="clearfix"> </div>
 						   </div>
 						   <div className="container for-emp-right">
 						      <div className="col-md-4  ">
-
+						         
 						      </div>
 						      <div className="col-md-8  ">
-						         <div  className="alisetright">
+						         <div  className="alisetright">					
 						            <img src="images/laptopimg.jpg"  className="img-responsive"/>
 						         </div>
 						      </div>
 						      <div className="clearfix"> </div>
 						   </div>
 						</div>
-
+                         
                          <Footer /></div>
 				       );
-
+         
             }
 
 }
