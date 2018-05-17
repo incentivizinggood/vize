7c49b2fbb307b8f21970ce2383def65e3c701034
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-05-17 16:00:21 -0500

    aUndid changes to server/main.js

--- server/main.js
+++ server/main.js
@@ -4,12 +4,11 @@ import { Meteor } from "meteor/meteor";
 // These files need to be run both on the client(s) and server.
 import "../imports/api/data/reviews.js";
 import "../imports/api/data/companies.js";
-import { Email } from 'meteor/email'
 import "../imports/api/data/salaries.js";
 import "../imports/api/data/users.js";
 import "../imports/api/data/jobads.js";
 
-// BUG FIXED For real guys, you can't use Methods defined
+//BUG FIXED For real guys, you can't use Methods defined
 //in methods.js unless you include them here. I'm thinking
 //we have a lot to learn about loading order in Meteor.
 import "../imports/api/data/methods.js";
@@ -17,4 +16,5 @@ import "../imports/api/data/methods.js";
 
 Meteor.startup(() => {
 	// code to run on server at startup
+	
 });
