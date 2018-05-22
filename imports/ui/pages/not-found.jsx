2a7e623b9c03072e760e3632a688ae749b7d0c2b
joshuatshaffer <joshuatshaffer@gmail.com>
2018-05-22 15:33:46 -0500

    Fixed not-found.jsx.

--- imports/ui/pages/not-found.jsx
+++ imports/ui/pages/not-found.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 /* The "404" page.
- * Note: This is not technicaly a 404 page because the server
- *       is not actualy sending a 404 error to an HTTP reqest.
  */
-export default class NotFoundPage extends React.Component {
-	render() {
-		return (
-			<div className="page not-found">
-				<h1>404</h1>
-				<h2>That page could not be found.</h2>
-			</div>
-		);
-	}
+export default function NotFoundPage() {
+	return (
+		<div className="page not-found">
+			<h1>404</h1>
+			<h2>That page could not be found.</h2>
+		</div>
+	);
 }
