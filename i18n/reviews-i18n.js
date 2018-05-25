59cb36244653124187365c86e0eda177da046d7d
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-05-25 16:23:31 -0500

    Removed some test-only code

--- i18n/reviews-i18n.js
+++ i18n/reviews-i18n.js
@@ -95,11 +95,12 @@ const reviewErrorMessages = function(locale) {
 const englishReviews = reviewErrorMessages("en");
 const spanishReviews = reviewErrorMessages("es");
 
-console.log("english review error messages");
-console.log(englishReviews);
-console.log("spanish review error messages");
-console.log(spanishReviews);
-
+if (Meteor.isDevelopment) {
+	console.log("english review error messages");
+	console.log(englishReviews);
+	console.log("spanish review error messages");
+	console.log(spanishReviews);
+}
 Reviews.schema.labels(reviewLabels());
 
 Reviews.schema.messageBox.messages({
@@ -108,8 +109,7 @@ Reviews.schema.messageBox.messages({
 });
 
 i18n.onChangeLocale(function(newLocale) {
-	// if (Meteor.isDevelopment) console.log("REVIEWS: " + newLocale);
-	console.log("REVIEWS: " + newLocale);
+	if (Meteor.isDevelopment) console.log("REVIEWS: " + newLocale);
 	Reviews.schema.messageBox.setLanguage(newLocale);
 	Reviews.schema.labels(reviewLabels());
 });
