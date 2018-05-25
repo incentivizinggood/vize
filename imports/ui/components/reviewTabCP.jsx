38b1a329236e6fdf313fef414de23caf459a1a2a
krittt <krit.nsit@gmail.com>
2018-05-25 13:56:17 -0500

    did the review_Tab

--- imports/ui/components/reviewTabCP.jsx
+++ imports/ui/components/reviewTabCP.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import StarRatings from "react-star-ratings";
 import CompanyReview from "../../ui/components/companyReview.jsx";
 import CompanyRating from "../../ui/components/companyRatingsComponent.jsx";
+import i18n from "meteor/universe:i18n";
+
+const T = i18n.createComponent();
 
 export default class ReviewTab extends React.Component {
 	renderItems() {
@@ -16,7 +19,8 @@ export default class ReviewTab extends React.Component {
 			<div role="tabpanel" className="tab-pane" id="reviews">
 				<div className="col-md-12  section_rview_back_color2  ">
 					<h4 className="head_section_font">
-						{this.props.companyinfo.name} Reviews
+						{this.props.companyinfo.name}{" "}
+						<T>common.review_tab.reviews</T>
 					</h4>
 					<div className="add-buttons">
 						<a
@@ -24,8 +28,8 @@ export default class ReviewTab extends React.Component {
 							className="btn btn-primary"
 						>
 							{" "}
-							<i className="fa fa-plus" aria-hidden="true" /> Add
-							a Review
+							<i className="fa fa-plus" aria-hidden="true" />{" "}
+							{i18n.__("common.review_tab.add_review")}
 						</a>
 						{/* <button ><i className="fa fa-plus" ></i>&nbsp; Add a Review</button> */}
 					</div>
