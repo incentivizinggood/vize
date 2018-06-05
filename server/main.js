f1f4223cdc66e7adee4a9fc5aef204f7719330be
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-05 12:54:36 -0500

    Moved server GraphQL startup to its own file.

--- server/main.js
+++ server/main.js
@@ -1,10 +1,6 @@
 import { Meteor } from "meteor/meteor";
-import { createApolloServer } from "meteor/apollo";
-import { makeExecutableSchema } from "graphql-tools";
 
-import { typeDefs } from "/imports/api/graphql/schema.js";
-import { resolvers } from "/imports/api/graphql/resolvers.js";
-
+import "../imports/startup/server/graphql.js";
 // Import all of the collection and method definitions.
 // These files need to be run both on the client(s) and server.
 import "../imports/api/data/reviews.js";
@@ -14,15 +10,6 @@ import "../imports/api/data/users.js";
 import "../imports/api/data/jobads.js";
 import "../imports/api/data/votes.js";
 import "../imports/api/data/methods.js";
-
-const schema = makeExecutableSchema({
-	typeDefs,
-	resolvers,
-});
-
-createApolloServer({
-	schema,
-});
 
 Meteor.startup(() => {
 	// code to run on server at startup
