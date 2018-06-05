80fb17be750c0e6224fb170f176880655cf0f16f
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-05 14:25:15 -0500

    Started using a package to put GraphQL schemas in a file instead of a string.

--- imports/startup/server/graphql.js
+++ imports/startup/server/graphql.js
@@ -1,7 +1,7 @@
 import { createApolloServer } from "meteor/apollo";
 import { makeExecutableSchema } from "graphql-tools";
 
-import { typeDefs } from "/imports/api/graphql/schema.js";
+import typeDefs from "/imports/api/graphql/schema.graphql";
 import { resolvers } from "/imports/api/graphql/resolvers.js";
 
 const schema = makeExecutableSchema({
