fc55600def4ebad0f821011ddc4f3faeded85c32
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-05 13:42:33 -0500

    Added users to GraphQL.

--- imports/api/graphql/resolvers.js
+++ imports/api/graphql/resolvers.js
@@ -1,12 +1,23 @@
+import { Meteor } from "meteor/meteor";
+
+/* eslint-disable no-unused-vars */
 export const resolvers = {
 	Query: {
 		say(root, args, context) {
-			/*
-			 * We access to the current user here thanks to the context. The current
-			 * user is added to the context thanks to the `meteor/apollo` package.
-			 * return context.user;
-			 */
 			return "Hello world.";
 		},
+		user(root, args, context) {
+			return Meteor.users.findOne(args.id);
+		},
+		currentUser(root, args, context) {
+			// The current user is added to the context
+			// by the `meteor/apollo` package.
+			return context.user;
+		},
+	},
+	User: {
+		id: ({ _id }) => _id,
+		username: ({ username }) => username,
+		role: ({ role }) => role.toUpperCase().replace("-", "_"),
 	},
 };
--- imports/api/graphql/schema.js
+++ imports/api/graphql/schema.js
@@ -1,7 +1,21 @@
 export const typeDefs = [
 	`
+enum UserRole {
+	WORKER
+	COMPANY
+	COMPANY_UNVERIFIED
+}
+
+type User {
+	id: String!
+	username: String!
+	role: UserRole!
+}
+
 type Query {
 	say: String!
+	user(id: ID!): User
+	currentUser: User
 }
 `,
 ];
