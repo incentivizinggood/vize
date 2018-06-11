a9e918dbacb70b9cef0f0655e0c06107b80314d8
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-11 13:18:13 -0500

    Attempted to write unit tests for the models.

---
+++ imports/api/models/models.js
@@ -0,0 +1,47 @@
+import { Meteor } from "meteor/meteor";
+
+// Import models.
+import CommentModel from "/imports/api/models/comment.js";
+import CompanyModel from "/imports/api/models/company.js";
+import JobAdModel from "/imports/api/models/job-ad.js";
+import ReviewModel from "/imports/api/models/review.js";
+import SalaryModel from "/imports/api/models/salary.js";
+import UserModel from "/imports/api/models/user.js";
+import VoteModel from "/imports/api/models/vote.js";
+
+// Import connectors
+import { Comments } from "./../../api/data/comments.js";
+import { Companies } from "./../../api/data/companies.js";
+import { JobAds } from "./../../api/data/jobads.js";
+import { Reviews } from "./../../api/data/reviews.js";
+import { Salaries } from "./../../api/data/salaries.js";
+// The users connector is Meteor.users which is imported above.
+import { Votes } from "./../../api/data/votes.js";
+
+function createConnectors() {
+	const connectors = {
+		commentConnector: Comments,
+		companyConnector: Companies,
+		jobAdConnector: JobAds,
+		reviewConnector: Reviews,
+		salaryConnector: Salaries,
+		userConnector: Meteor.users,
+		voteConnector: Votes,
+	};
+	return connectors;
+}
+
+// This object constructing function is used to help pass references around.
+export default function constructModels(connectors = createConnectors()) {
+	const models = {
+		commentModel: new CommentModel(connectors.commentConnector),
+		companyModel: new CompanyModel(connectors.companyConnector),
+		jobAdModel: new JobAdModel(connectors.jobAdConnector),
+		reviewModel: new ReviewModel(connectors.reviewConnector),
+		salaryModel: new SalaryModel(connectors.salaryConnector),
+		userModel: new UserModel(connectors.userConnector),
+		voteModel: new VoteModel(connectors.voteConnector),
+	};
+	Object.values(models).forEach(model => model.init(models));
+	return models;
+}
