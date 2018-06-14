f68c41f759b9b18444f418e710698fd27afad435
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-14 13:40:48 -0500

    Simplified review schema error message i18n.

--- i18n/reviews-i18n.js
+++ i18n/reviews-i18n.js
@@ -1,42 +1,23 @@
-import { Reviews } from "../imports/api/data/reviews.js";
+import merge from "lodash.merge";
 import i18n from "meteor/universe:i18n";
+import { Reviews } from "../imports/api/data/reviews.js";
 
-const reviewErrorMessages = function(locale) {
-	return {
-		needsFiveWords: i18n.__("SimpleSchema.custom.Reviews.needsFiveWords", {
-			_locale: locale,
-		}),
-		noCompanyWithThatName: i18n.__(
-			"SimpleSchema.custom.noCompanyWithThatName",
-			{ _locale: locale }
-		),
-		sessionError: i18n.__("SimpleSchema.custom.sessionError", {
-			_locale: locale,
-		}),
-	};
-};
-
-const englishReviews = reviewErrorMessages("en");
-const spanishReviews = reviewErrorMessages("es");
-
-if (Meteor.isDevelopment) {
-	console.log(Meteor.isServer ? "SERVER env" : "CLIENT env");
-	console.log(process.env.UNIVERSE_I18N_LOCALES);
-	console.log("english review error messages");
-	console.log(englishReviews);
-	console.log("spanish review error messages");
-	console.log(spanishReviews);
-}
-Reviews.schema.labels(i18n.getTranslations("SimpleSchema.labels.Reviews"));
-
-Reviews.schema.messageBox.messages({
-	en: englishReviews,
-	es: spanishReviews,
+const reviewErrorMessages = locale => ({
+	[locale]: merge(
+		i18n.getTranslations("SimpleSchema.custom", locale),
+		i18n.getTranslations("SimpleSchema.custom.Reviews", locale)
+	),
 });
+
+Reviews.schema.messageBox.messages(reviewErrorMessages("en"));
+Reviews.schema.messageBox.setLanguage("en");
+Reviews.schema.labels(i18n.getTranslations("SimpleSchema.labels.Reviews"));
 
 i18n.onChangeLocale(function(newLocale) {
 	if (Meteor.isDevelopment) console.log("REVIEWS: " + newLocale);
 	// console.log("REVIEWS: " + newLocale);
+	console.log(reviewErrorMessages(newLocale));
+	Reviews.schema.messageBox.messages(reviewErrorMessages(newLocale));
 	Reviews.schema.messageBox.setLanguage(newLocale);
 	Reviews.schema.labels(i18n.getTranslations("SimpleSchema.labels.Reviews"));
 });
