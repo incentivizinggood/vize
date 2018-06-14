4043604a8bf5303ce757562a613f259ea1a91146
joshuatshaffer <joshuatshaffer@gmail.com>
2018-06-14 15:24:46 -0500

    Simplified default error messages code.

--- i18n/1defaultErrors-i18n.js
+++ i18n/1defaultErrors-i18n.js
@@ -1,6 +1,7 @@
 import SimpleSchema from "simpl-schema";
 import i18n from "meteor/universe:i18n";
 import find from "lodash.find";
+import merge from "lodash.merge";
 
 const regExpMessages = function(locale) {
 	return Object.keys(SimpleSchema.RegEx).map(key => ({
@@ -12,54 +13,15 @@ const regExpMessages = function(locale) {
 };
 
 const errorMessages = function(locale) {
-	const errors = {
-		required: i18n.__("SimpleSchema.defaults.required", {
-			_locale: locale,
-		}),
-		minString: i18n.__("SimpleSchema.defaults.minString", {
-			_locale: locale,
-		}),
-		maxString: i18n.__("SimpleSchema.defaults.maxString", {
-			_locale: locale,
-		}),
-		minNumber: i18n.__("SimpleSchema.defaults.minNumber", {
-			_locale: locale,
-		}),
-		maxNumber: i18n.__("SimpleSchema.defaults.maxNumber", {
-			_locale: locale,
-		}),
-		minNumberExclusive: i18n.__(
-			"SimpleSchema.defaults.minNumberExclusive",
-			{ _locale: locale }
-		),
-		maxNumberExclusive: i18n.__(
-			"SimpleSchema.defaults.maxNumberExclusive",
-			{ _locale: locale }
-		),
-		minDate: i18n.__("SimpleSchema.defaults.minDate", { _locale: locale }),
-		maxDate: i18n.__("SimpleSchema.defaults.maxDate", { _locale: locale }),
-		badDate: i18n.__("SimpleSchema.defaults.badDate", { _locale: locale }),
-		minCount: i18n.__("SimpleSchema.defaults.minCount", {
-			_locale: locale,
-		}),
-		maxCount: i18n.__("SimpleSchema.defaults.maxCount", {
-			_locale: locale,
-		}),
-		noDecimal: i18n.__("SimpleSchema.defaults.noDecimal", {
-			_locale: locale,
-		}),
-		notAllowed: i18n.__("SimpleSchema.defaults.notAllowed", {
-			_locale: locale,
-		}),
-		expectedType: i18n.__("SimpleSchema.defaults.expectedType", {
-			_locale: locale,
-		}),
+	return merge(i18n.getTranslations("SimpleSchema.defaults"), {
 		regEx({ label, regExp }) {
 			// See if there's one where exp matches this expression
 			let msgObj;
-			let rem = regExpMessages(locale);
 			if (regExp) {
-				msgObj = find(rem, o => o.exp && o.exp.toString() === regExp);
+				msgObj = find(
+					regExpMessages(locale),
+					o => o.exp && o.exp.toString() === regExp
+				);
 			}
 
 			const regExpMessage = msgObj
@@ -70,12 +32,7 @@ const errorMessages = function(locale) {
 
 			return `${label} ${regExpMessage}`;
 		},
-
-		keyNotInSchema: i18n.__("SimpleSchema.defaults.keyNotInSchema", {
-			_locale: locale,
-		}),
-	};
-	return errors;
+	});
 };
 
 const englishDefaults = errorMessages("en");
