536e0e511e77a06ba506b79b176c46f346140329
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-15 15:15:34 -0500

    Changed a file name.

--- server/sql/tests/alltests.sql
+++
@@ -1,5 +0,0 @@
-\i ./server/sql/wipedb.sql;
-\i ./server/sql/init/init-db.sql;
-\i ./server/sql/tests/acceptance.sql;
-\i ./server/sql/tests/invalid-args.sql;
-\i ./server/sql/tests/invalid-regex.sql;
