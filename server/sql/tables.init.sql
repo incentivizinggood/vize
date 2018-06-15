1720fd5576000e5e3c33e6bb189ae7bc9da9fc15
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-14 20:39:51 -0500

    Starting to write functions and figure out how to use them

--- server/sql/tables.init.sql
+++ server/sql/tables.init.sql
@@ -16,12 +16,10 @@ CREATE TABLE companies (
 	industry			varchar(60),
 	otherContactInfo	varchar(255),
 	descriptionOfCompany	text,
-	-- locations	-- can be done either with a separate table or dynamic columns, will probably use a separate table
 
 	-- Other validity onstraints are straightforward via CHECK,
 	-- I love that PostgreSQL actually supports this
-
-	-- defines allowed brackets for numEmployees
+	-- allowed brackets for numEmployees
 	numEmployees		varchar(20)		CHECK (numEmployees IS NULL OR numEmployees='1 - 50' OR numEmployees='51 - 500' OR numEmployees='501 - 2000' OR numEmployees='2001 - 5000' OR numEmployees='5000+'),
 	-- regex CHECK constraint for email (with TLD) validity
 	-- QUESTION Should I find a better place to keep the regex strings?
@@ -48,41 +46,3 @@ CREATE TABLE locations (
 	locationName		varchar(190),
 	PRIMARY KEY (companyName, locationName)
 );
-
--- Okay, here's the tricky part:
--- One-many relationship from companies to locations,
--- many-one from locations to companies. Solved on
--- locations side by foreign key, but how to make sure
--- that each company has at least one location?
-
--- SOLUTION:
--- Foreign key as it currently stands, plus
--- "deferred constraint triggers", one for each
--- case (after update/delete/truncate on locations,
--- after insert on companies, constraint triggers
--- can only be after but it's fine because they
--- roll back the transaction).
-
--- QUESTION
--- What language to write triggers in? PostgreSQL doesn't
--- support full trigger definitions via DDL like Maria does.
--- Options: C (requires clunky setup, harder to secure)
---			pgSQL (fine, but not very powerful)
---			tcl (fine, but would have to learn)
---			Python (great, but not-fully-supported by postgres and seems very insecure)
---			Perl (great, well-supported and secure but hard to read,
---			and would have to learn)
--- SCRATCH ALL THAT
--- I would have gone with Perl, but the learning curve seems really steep.
--- Will see how far I get with pl/V8 (Javascript) via CREATE EXTENSION,
--- because this stuff just needs to get done.
--- Why? It's a "trusted" (doesn't have to run as root) language that
--- I already know and provides all the necessary power, we just need to install
--- it, which means breaking out of the Meteor ecosystem a bit.
-
--- WARNING
--- If this succeeds, the Vize Web App project will depend on PL/V8,
--- which will have to be manually installed on Galaxy by...probably me.
--- Although I guess it wouldn't be hard to just clone the repo and
--- "make install", which is basically what the instructions on their
--- website tell you to do.
