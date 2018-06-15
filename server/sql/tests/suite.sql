d20089f4797799194ac219363d0b91e338e88c57
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-15 15:15:49 -0500

    ...and added it back in

---
+++ server/sql/tests/suite.sql
@@ -0,0 +1,5 @@
+\i ./server/sql/wipedb.sql;
+\i ./server/sql/init/init-db.sql;
+\i ./server/sql/tests/acceptance.sql;
+\i ./server/sql/tests/invalid-args.sql;
+\i ./server/sql/tests/invalid-regex.sql;
