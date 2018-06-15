7c7cb5647e906f9f8b01e1b9b851f9e2e82dc538
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-15 15:14:17 -0500

    Restructured SQL scripts again.

--- server/sql/tests/alltests.sql
+++ server/sql/tests/alltests.sql
@@ -1,5 +1,5 @@
-\i /home/jhigginbotham64/Desktop/vize/vize-meteor-app/server/sql/wipedb.sql;
-\i /home/jhigginbotham64/Desktop/vize/vize-meteor-app/server/sql/db.init.sql;
-\i /home/jhigginbotham64/Desktop/vize/vize-meteor-app/server/sql/tests/acceptance.sql;
-\i /home/jhigginbotham64/Desktop/vize/vize-meteor-app/server/sql/tests/invalid-args.sql;
-\i /home/jhigginbotham64/Desktop/vize/vize-meteor-app/server/sql/tests/invalid-regex.sql;
+\i ./server/sql/wipedb.sql;
+\i ./server/sql/init/init-db.sql;
+\i ./server/sql/tests/acceptance.sql;
+\i ./server/sql/tests/invalid-args.sql;
+\i ./server/sql/tests/invalid-regex.sql;
