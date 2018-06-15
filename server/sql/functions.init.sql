2af2235a6f1d3a2085b0108b4ea08755ce9bd4be
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-15 15:06:53 -0500

    Regex constraints seem to be working. Did I mention that I love PostgreSQL?

--- server/sql/functions.init.sql
+++ server/sql/functions.init.sql
@@ -1,23 +1,23 @@
--- (contactEmail ~ '^(([^<>()\\[\\]\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'),
-
+-- regex check for email w/ TLD
 CREATE OR REPLACE FUNCTION is_valid_email_with_tld(arg text)
 RETURNS boolean AS
 $$
-	plv8.elog(NOTICE, "Inside the email regex function");
-	return true;
+	const emailWithTldRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return emailWithTldRegex.test(arg);
 $$ LANGUAGE plv8;
 
--- (websiteURL ~ '^(?:(?:https?|ftp):\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x00a1-\\xffff0-9]+-?)*[a-z\\x00a1-\\xffff0-9]+)(?:\\.(?:[a-z\\x00a1-\\xffff0-9]+-?)*[a-z\\x00a1-\\xffff0-9]+)*(?:\\.(?:[a-z\\x00a1-\\xffff]{2,})))(?::\\d{2,5})?(?:\\/[^\\s]*)?$')
-
+-- regex check for URL
 CREATE OR REPLACE FUNCTION is_valid_url(arg text)
 RETURNS boolean AS
 $$
-	plv8.elog(NOTICE, "Inside the URL regex function");
-	return true;
+	const urlRegex=/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+	return urlRegex.test(arg);
 $$ LANGUAGE plv8;
 
+-- trigger function to hackishly kind-of-enforce a foreign key constraint
+-- on companies, our workaround for not being in Meteor/Mongo where we
+-- can specify that array fields must have a minimum number of elements
 CREATE OR REPLACE FUNCTION check_company_locations() RETURNS TRIGGER AS
 $$
-	plv8.elog(NOTICE, "Hello, world of PostgreSQL triggers!");
 	return NEW;
 $$ LANGUAGE plv8;
--- server/sql/tests/invalid-regex.sql
+++ server/sql/tests/invalid-regex.sql
@@ -1,1 +1,35 @@
-\! echo "INVALID REGEX TEST 1: "
+\! echo "INVALID REGEX TEST 1: (Email with TLD)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test1', '1 - 50', 'examplegmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 2: (Email with TLD)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test2', '1 - 50', 'example@gmailcom', 'https://example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 3: (Email with TLD)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test3', '1 - 50', 'example@gmail.c', 'https://example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 4: (Email with TLD)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test4', '1 - 50', '@gmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 5: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test5', '1 - 50', 'example@gmail.com', 'https//example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 6: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test6', '1 - 50', 'example@gmail.com', 'https://examplecom',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 7: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test7', '1 - 50', 'example@gmail.com', 'https:/example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 8: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test8', '1 - 50', 'example@gmail.com', 'https:example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 9: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test9', '1 - 50', 'example@gmail.com', 'https://example.c',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 10: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test10', '1 - 50', 'example@gmail.com', 'https://.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 11: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test11', '1 - 50', 'example@gmail.com', 'fttps://example.com',0,0,0,0,0,0,0,0,0);
+
+\! echo "INVALID REGEX TEST 12: (URL)"
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('invalid-regex-test12', '1 - 50', 'example@gmail.com', '://example.com',0,0,0,0,0,0,0,0,0);
