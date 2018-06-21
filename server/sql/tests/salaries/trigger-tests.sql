27ead7e16f1d13ed22016d6e59c9d5b0fa65bee7
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-21 20:32:26 +0000

    Added foreign key tests for salaries. That should be all for salaries. However, I've realized that I need to go back and fill in some small test-coverage gaps: companyId foreign keys, untested invalid args (mostly for companies), and perhaps some other things. Also I need to go and fix the varchar field lengths to be closer to the SimpleSchema versions.

---
+++ server/sql/tests/salaries/trigger-tests.sql
@@ -0,0 +1,26 @@
+\! echo "--- TESTING SALARY-RELATED FUNCTIONALITY *WITH* TRIGGERS ---"
+\i ./server/sql/tests/wipedb.sql;
+\i ./server/sql/init/init-db.sql;
+
+-- test salaries foreign key to companies
+-- this one should pass
+START TRANSACTION;
+INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('a', '1 - 50', 'example@gmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
+INSERT INTO company_locations(companyId,locationName) VALUES (1,'somewhere over the rainbow'),(1,'hello world'),(1,'anotherwhere'),(1,'movin right along');
+INSERT INTO salaries
+(submittedBy,companyName,
+	jobTitle,incomeType,incomeAmount)
+	VALUES (0,'a','a','Hourly Wage',0);
+COMMIT;
+
+-- fail: no company 'b'
+INSERT INTO salaries
+(submittedBy,companyName,
+	jobTitle,incomeType,incomeAmount)
+	VALUES (0,'b','a','Hourly Wage',0);
+
+-- fail: no company with id -1
+INSERT INTO salaries
+(submittedBy,companyName,companyId,
+	jobTitle,incomeType,incomeAmount)
+	VALUES (0,'a',-1,'a','Hourly Wage',0);
