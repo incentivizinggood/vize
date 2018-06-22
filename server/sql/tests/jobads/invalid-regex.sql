5a56964424e6aa8e1c0abd18e2fd2e2ba0ec85fb
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-22 13:08:50 +0000

    Fixed bugs in acceptance and invalid-args tests for jobads, still working on regex tests.

---
+++ server/sql/tests/jobads/invalid-regex.sql
@@ -0,0 +1,4 @@
+\! echo "INVALID REGEX TEST 1: (Pay Range)"
+INSERT INTO jobads(companyName,jobTitle,pesosPerHour,contractType,
+				jobDescription,responsibilities,qualifications)
+	VALUES ('a','a','0','Part time','a','a','a');
