148f60dfc23830aa2c8ef03d306455257b321489
Urel <perfect123@urels-mbp.attlocal.net>
2018-06-24 10:19:36 -0500

    New error needs to be fixed

--- imports/ui/pages/login.jsx
+++ imports/ui/pages/login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { FlowRouter } from "meteor/kadira:flow-router";
 import i18n from "meteor/universe:i18n";
+import Header from "../../ui/pages/header.jsx";
+import Footer from "../../ui/pages/footer.jsx";
 
 const t = i18n.createTranslator("common.login");
 const T = i18n.createComponent(t);
@@ -79,33 +81,67 @@ export default class LoginPage extends React.Component {
 						<T>{`error.${this.state.error}`}</T>
 					</div>
 				) : null}
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="loginform-username">
-						<T>username</T>
-						<input
-							id="loginform-username"
-							name="username"
-							type="text"
-							placeholder={t("username")}
-							required
-							value={this.state.username}
-							onChange={this.handleInputChange}
-						/>
-					</label>
-					<label htmlFor="loginform-password">
-						<T>password</T>
-						<input
-							id="loginform-password"
-							name="password"
-							type="password"
-							placeholder={t("password")}
-							required
-							value={this.state.password}
-							onChange={this.handleInputChange}
-						/>
-					</label>
-					<input type="submit" value={t("submit")} />
-				</form>
+				<Header/>
+				<div className="container  login-top-spce">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-3">
+							<div className="panel panel-login">
+								<div className="panel-heading">
+									<div className="row">
+										<div className="col-xs-12">
+											<br/>
+												<h3 className="top-head-employer" align="center">Login</h3>
+												<hr/>
+										</div>
+									</div>
+								</div>
+								<div className="panel-body">
+									<div className="row">
+										<div className="col-lg-12">
+											<form id="login-form" action="#" method="post" role="form" style={{display: "block"}} onSubmit={this.handleSubmit}>
+												<div className="login-fm">
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="text" placeholder="Username" className="form-control  "  name="username" id="username" placeholder={t("username")} required value={this.state.username} onChange={this.handleInputChange}/>
+															<label htmlFor="loginform-username" for="Username" className="glyphicon glyphicon-user" rel="tooltip" title="Username"/>
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="password" name="password" id="password" className="form-control"  placeholder="Password" value="" placeholder={t("password")} required value={this.state.password} onChange={this.handleInputChange}/>
+															<label for="password" className="fa fa-lock" rel="tooltip" title="password"/>
+														</div>
+													</div>
+
+													<div className="form-group text-center">
+													</div>
+
+													<div className="button-center">
+														<a href="#" className="button out-bodr-login" type="submit" value={t("submit")}>Login</a>
+													</div>
+
+
+													<div className="form-group">
+														<div className="row">
+															<div className="col-lg-12">
+																<div className="text-center reg">
+																	Don't have an account?  <a href="/register" > Register</a>
+																</div>
+																<br/>
+															</div>
+														</div>
+													</div>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Footer/>
 			</div>
 		);
 	}
--- imports/ui/pages/register.jsx
+++ imports/ui/pages/register.jsx
@@ -3,6 +3,10 @@ import { Meteor } from "meteor/meteor";
 import { FlowRouter } from "meteor/kadira:flow-router";
 import { Accounts } from "meteor/accounts-base";
 import i18n from "meteor/universe:i18n";
+import Header from "../../ui/pages/header.jsx";
+import Footer from "../../ui/pages/footer.jsx";
+import Dialog from "./dialog-box.jsx"
+
 
 const t = i18n.createTranslator("common.register");
 const T = i18n.createComponent(t);
@@ -37,15 +41,16 @@ export default class RegisterPage extends React.Component {
 	}
 
 	handleInputChange(event) {
-		const { target } = event;
+		const {target} = event;
 		const value =
 			target.type === "checkbox" ? target.checked : target.value;
-		const { name } = target;
+		const {name} = target;
 
 		this.setState({
 			[name]: value,
 		});
 	}
+
 
 	handleSubmit(event) {
 		event.preventDefault(); // Prevent the default behavior for this event.
@@ -69,73 +74,149 @@ export default class RegisterPage extends React.Component {
 		Accounts.createUser(options, createUserCallback);
 	}
 
+	buttonClicked(event) {
+		event.preventDefault();
+		$(document).ready(function() {
+			$('#register-form-link').click(function(e) {
+				$("#register-form").delay(100).fadeIn(100);
+				$("#worker-form").fadeOut(100);
+				$('#worker-form-link').removeClass('active');
+				$(this).addClass('active');
+				e.preventDefault();
+			});
+
+			$('#worker-form-link').click(function(e) {
+				$("#worker-form").delay(100).fadeIn(100);
+				$("#register-form").fadeOut(100);
+				$('#register-form-link').removeClass('active');
+				$(this).addClass('active');
+				e.preventDefault();
+			});
+		});
+	}
+
 	render() {
 		if (this.state.success) {
-			return (
-				<div className="page register">
-					<T>success</T>
-				</div>
-			);
+			return <div className="page register">Sign up successful!</div>;
 		}
 		return (
+
 			<div className="page register">
-				{this.state.error ? (
-					<div>
-						<T>{`error.${this.state.error}`}</T>
+				{this.state.error ? <div>{this.state.error}</div> : null}
+				<Header/>
+				<div className="container  login-top-spce">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-3">
+							<div className="panel panel-login   register-work-employee">
+								<div className="panel-body">
+									<div className="row">
+										<div className="col-lg-12">
+											<form id="register-form" action="" method="post" style= {{display: "none"}} onSubmit={this.handleSubmit}>
+												<h3 className="top-head-employer" align="center">Employer Register</h3>
+												<br/>
+												<div  className="employer-fm">
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="text" placeholder="Username" className="form-control"  name="username" id="username" autoFocus
+																   required
+																   value={this.state.username}
+																   onChange={this.handleInputChange}/>
+															<label for="Username" className="glyphicon glyphicon-user" rel="tooltip" title="Username" />
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="email" name="email" id="email" className="form-control"  placeholder="Email " value="" />
+															<label for="email" className="fa fa-envelope-o" rel="tooltip" title="email" />
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="text" name="companyName" id="companyName" className="form-control"  placeholder="Company Name"  value=""/>
+															<label for="companyName" className="fa fa-building-o" rel="tooltip" title="companyName"/>
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md   pwd-line-sm">
+															<input type="password" name="password" id="password" className="form-control"  placeholder="Password" value="" required
+																   value={this.state.password}
+																   onChange={this.handleInputChange}/>
+															<label for="password" className="fa fa-lock" rel="tooltip" title="password"/>
+														</div>
+													</div>
+
+													<div className="button-center">
+														<a href="#" className="button out-bodr-get1">Get Started</a>
+													</div>
+												</div>
+											</form>
+											<form id="worker-form" action="" method="post" role="form"   style={{display: "block"}}>
+												<h3  className="top-head-worker" align="center" >Worker  Register</h3>
+												<br/>
+												<div  className="employer-fm">
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="text" placeholder="Username" className="form-control  "  name="username" id="username" autoFocus
+																   required
+																   value={this.state.username}
+																   onChange={this.handleInputChange}/>
+															<label for="Username" className="glyphicon glyphicon-user" rel="tooltip" title="Username" />
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md">
+															<input type="email" name="email" id="email" className="form-control  "  placeholder="Email " value="" />
+															<label for="email" className="fa fa-envelope-o" rel="tooltip" title="email"/>
+														</div>
+													</div>
+
+													<div className="form-group">
+														<div className="icon-addon addon-md   pwd-line-sm">
+															<input type="password" name="password" id="password" className="form-control  "  placeholder="Password" value="" required
+																   value={this.state.password}
+																   onChange={this.handleInputChange}/>
+															<label for="password" className="fa fa-lock" rel="tooltip" title="password" />
+														</div>
+													</div>
+												</div>
+
+												<div className="button-center">
+													<a href="#" className="button out-bodr-get1">Get Started</a>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+								<div className="panel-heading  p-head">
+									<div className="row">
+										<div className="col-lg-12">
+											<div className="text-center   login-link-cs">
+												Already have an account?  <a href="/login" > Log In </a>
+											</div>
+											<div className="clearfix" />
+										</div>
+										<div className="col-xs-12 ">
+											<div className="btn-emp">
+												<a href="#"   className="button" id="register-form-link"  role="button" onClick={this.buttonClicked} >Employer</a>
+											</div>
+											<div  className="btn-work">
+												<a href="#" className="button  active" id="worker-form-link"  role="button" onClick={this.buttonClicked} >Worker  </a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-				) : null}
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="registerform-username">
-						<T>username</T>
-						<input
-							id="registerform-username"
-							name="username"
-							type="text"
-							placeholder={t("username")}
-							required
-							value={this.state.username}
-							onChange={this.handleInputChange}
-						/>
-					</label>
-					<label htmlFor="registerform-password">
-						<T>password</T>
-						<input
-							id="registerform-password"
-							name="password"
-							type="password"
-							placeholder={t("password")}
-							required
-							value={this.state.password}
-							onChange={this.handleInputChange}
-						/>
-					</label>
-					<label htmlFor="registerform-role-worker">
-						<input
-							id="registerform-role-worker"
-							name="role"
-							type="radio"
-							required
-							value="worker"
-							checked={this.state.role === "worker"}
-							onChange={this.handleInputChange}
-						/>
-						<T>role.worker</T>
-					</label>
-					<label htmlFor="registerform-role-company">
-						<input
-							id="registerform-role-company"
-							name="role"
-							type="radio"
-							required
-							value="company"
-							checked={this.state.role === "company"}
-							onChange={this.handleInputChange}
-						/>
-						<T>role.company</T>
-					</label>
-					<input type="submit" value={t("submit")} />
-				</form>
+				</div>
+				<Dialog/>
+				<Footer/>
 			</div>
 		);
 	}
 }
+
