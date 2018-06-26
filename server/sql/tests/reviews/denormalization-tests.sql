d80b8ffd2adf6797ef55d712a3bee62bd8ca2ec4
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-06-26 17:15:29 +0000

    Polished up the newly-factored-out denormalization test script.

--- server/sql/tests/reviews/denormalization-tests.sql
+++ server/sql/tests/reviews/denormalization-tests.sql
@@ -2,87 +2,9 @@
 \i ./server/sql/wipedb.sql;
 \i ./server/sql/init/init-db.sql;
 
--- test reviews foreign key to companies
--- these should all be fine
 START TRANSACTION;
 INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('a', '1 - 50', 'example@gmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
-INSERT INTO company_locations(companyId,locationName) VALUES (1,'somewhere over the rainbow'),(1,'hello world'),(1,'anotherwhere'),(1,'movin right along');
-INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('b', '1 - 50', 'example@gmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
-INSERT INTO company_locations(companyId,locationName) VALUES (2,'somewhere over the rainbow'),(2,'hello world'),(2,'anotherwhere'),(2,'movin right along');
-INSERT INTO reviews
-(submittedBy,companyName,
-	reviewTitle,jobTitle,numMonthsWorked,
-	pros,cons,wouldRecommend,healthAndSafety,
-	managerRelationship,workEnvironment,benefits,
-	overallSatisfaction,additionalComments)
-	VALUES (0,'a','a','a',0,
-			'a a a a a','a a a a a',FALSE,
-			0,0,0,0,0,'Hello world!');
-INSERT INTO review_locations (reviewId,reviewLocation) VALUES (1,'hello world');
-INSERT INTO review_locations (reviewId,reviewLocation) VALUES (1,'anotherwhere');
-INSERT INTO reviews
-(submittedBy,companyName,
-	reviewTitle,jobTitle,numMonthsWorked,
-	pros,cons,wouldRecommend,healthAndSafety,
-	managerRelationship,workEnvironment,benefits,
-	overallSatisfaction,additionalComments)
-	VALUES (0,'b','a','a',0,
-			'a a a a a','a a a a a',FALSE,
-			0,0,0,0,0,'Hello world!');
-INSERT INTO review_locations (reviewId,reviewLocation) VALUES (2,'somewhere over the rainbow');
-COMMIT;
-
--- this next one should fail, no company 'c'
-INSERT INTO reviews
-(submittedBy,companyName,
-	reviewTitle,jobTitle,numMonthsWorked,
-	pros,cons,wouldRecommend,healthAndSafety,
-	managerRelationship,workEnvironment,benefits,
-	overallSatisfaction,additionalComments)
-	VALUES (0,'c','a','a',0,
-			'a a a a a','a a a a a',FALSE,
-			0,0,0,0,0,'Hello world!');
-
--- should fail, review has no locations
-START TRANSACTION;
-INSERT INTO reviews
-(submittedBy,companyName,
-	reviewTitle,jobTitle,numMonthsWorked,
-	pros,cons,wouldRecommend,healthAndSafety,
-	managerRelationship,workEnvironment,benefits,
-	overallSatisfaction,additionalComments)
-	VALUES (0,'b','a','a',0,
-			'a a a a a','a a a a a',FALSE,
-			0,0,0,0,0,'Hello world!');
-COMMIT;
-
--- should fail, would leave no locations for review
-DELETE FROM review_locations WHERE reviewId=1;
--- should succeed, one location left
-DELETE FROM review_locations WHERE reviewLocation='hello world';
--- these next two should both fail, as either
--- would remove a review's last location
-DELETE FROM review_locations WHERE reviewLocation='somewhere over the rainbow';
-UPDATE review_locations SET reviewId=1 WHERE reviewLocation='somewhere over the rainbow';
-
--- test comments foreign key to reviews
--- should be fine
-INSERT INTO review_comments (reviewId,submittedBy,content) VALUES (1,0,'hello world');
--- should fail
-INSERT INTO review_comments (reviewId,submittedBy,content) VALUES (3,0,'hello world');
-
--- should be pretty much all 0's on both of these,
--- but 1 review for each company
-select nummonthsworked,wouldrecommend,healthandsafety,managerrelationship,workenvironment,benefits,overallsatisfaction from reviews;
-select numreviews,avgnummonthsworked,percentrecommended,healthandsafety,managerrelationship,workenvironment,benefits,overallsatisfaction from companies;
-
--- should succeed via cascade to reviews and review_locations
-DELETE FROM companies *;
-
--- now to check to correctness of the denormalization
-START TRANSACTION;
-INSERT INTO companies (name,numEmployees,contactEmail,websiteURL,numFlags,numReviews, avgNumMonthsWorked,percentRecommended,healthAndSafety,managerRelationship,workEnvironment,benefits,overallSatisfaction) VALUES ('a', '1 - 50', 'example@gmail.com', 'https://example.com',0,0,0,0,0,0,0,0,0);
-INSERT INTO company_locations(companyId,locationName) VALUES (3,'somewhere over the rainbow');
+INSERT INTO company_locations(companyId,locationName) VALUES (1,'somewhere over the rainbow');
 INSERT INTO reviews
 (reviewId,submittedBy,companyName,
 	reviewTitle,jobTitle,numMonthsWorked,
