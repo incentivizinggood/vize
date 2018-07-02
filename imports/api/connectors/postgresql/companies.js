b02991ed4adee47951b0b3d718616ea16963229a
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-07-02 15:01:41 +0000

    First real attempt at 'simple' connector functions.

--- imports/api/connectors/postgresql/companies.js
+++ imports/api/connectors/postgresql/companies.js
@@ -1,18 +1,63 @@
 import { pool } from "./connection-pool.js";
 
 export default class CompanyConnector {
-	/*
-		getCompanyByName(name)
-		getCompanyById(id)
-		getCompanyLocations({id,name})
-		getCompanyStats({id,name})
-		companyNameRegexSearch(skip,limit)
+	static async getCompanyByName(name) {
+		const client = await pool.connect();
+		await client.query("START TRANSACTION READ ONLY");
+		const companyResults = await client.query(
+			"SELECT * FROM companies WHERE name=$1",
+			[name]
+		);
+		const locationResults = await client.query(
+			"SELECT locationname FROM company_locations WHERE companyid=$1",
+			[companyResults.companyid]
+		);
+		const statResults = await client.query(
+			"SELECT * FROM company_review_statistics WHERE companyname=$1",
+			[name]
+		);
+		await client.query("COMMIT");
+		client.release();
 
-		getAllCompanies(skip,limit) -> gets all companies, and assembles all
-							location and stat info as appropriate
+		return {
+			company: companyResults,
+			locations: locationResults,
+			review_stats: statResults,
+		};
+	}
 
-		createCompany -> creates company and inserts company locations as well
-		editCompany -> will need to be diligent about handling updated to company locations
-		deleteCompany -> should simple delete cascade?
-	*/
+	static async getCompanyById(id) {
+		const client = await pool.connect();
+		await client.query("START TRANSACTION READ ONLY");
+		const companyResults = await client.query(
+			"SELECT * FROM companies WHERE companyid=$1",
+			[id]
+		);
+		const locationResults = await client.query(
+			"SELECT locationname FROM company_locations WHERE companyid=$1",
+			[id]
+		);
+		const statResults = await client.query(
+			"SELECT * FROM company_review_statistics WHERE companyname=$1",
+			[companyResults.name]
+		);
+		await client.query("COMMIT");
+		client.release();
+
+		return {
+			company: companyResults,
+			locations: locationResults,
+			review_stats: statResults,
+		};
+	}
+
+	static async companyNameRegexSearch(text, skip, limit) {}
+
+	static async getAllCompanies(skip, limit) {}
+
+	static async createCompany(company) {}
+
+	static async editCompany(company) {}
+
+	static async deleteCompany(name) {}
 }
