440f6432b53a5d499f4996b6fa20eeee266479e2
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-07-13 18:54:03 +0000

    Review result processing function, and other associated changes, seem to be in good working order.

--- imports/api/models/helpers/postgresql/node-testing-setup.js
+++ imports/api/models/helpers/postgresql/node-testing-setup.js
@@ -537,7 +537,7 @@ submitReview = async function(client, review) {
 		review: newReview.rows[0],
 		// dummy values to prevent exception case
 		votes: {
-			refersto: newReview.reviewid,
+			refersto: newReview.rows[0].reviewid,
 			upvotes: 0,
 			downvotes: 0
 		}
@@ -553,6 +553,58 @@ processReviewResults = function(reviewResults) {
 		- review or reviews: singular review or array of reviews
 		- votes: singular or array depending on whether we get review or reviews
 	*/
+	if(reviewResults.review !== undefined) {
+		const review = reviewResults.review;
+		return {
+			_id: Number(review.reviewid),
+			submittedBy: Number(review.submittedby),
+			companyName: review.companyname,
+			companyId: Number(review.companyid),
+			reviewTitle: review.reviewtitle,
+			location: review.reviewlocation,
+			jobTitle: review.jobtitle,
+			numberOfMonthsWorked: Number(review.nummonthsworked),
+			pros: review.pros,
+			cons: review.cons,
+			wouldRecommendToOtherJobSeekers: review.wouldrecommend,
+			healthAndSafety: Number(review.healthandsafety),
+			managerRelationship: Number(review.managerrelationship),
+			workEnvironment: Number(review.workenvironment),
+			benefits: Number(review.benefits),
+			overallSatisfaction: Number(review.overallsatisfaction),
+			additionalComments: review.additionalcomments,
+			datePosted: review.dateadded,
+			upvotes: Number(reviewResults.votes.upvotes),
+			downvotes: Number(reviewResults.votes.downvotes)
+		}
+	}
+	else if(reviewResults.reviews !== undefined) {
+		return reviewResults.reviews.map(review => {
+			return {
+				_id: Number(review.reviewid),
+				submittedBy: Number(review.submittedby),
+				companyName: review.companyname,
+				companyId: Number(review.companyid),
+				reviewTitle: review.reviewtitle,
+				location: review.reviewlocation,
+				jobTitle: review.jobtitle,
+				numberOfMonthsWorked: Number(review.nummonthsworked),
+				pros: review.pros,
+				cons: review.cons,
+				wouldRecommendToOtherJobSeekers: review.wouldrecommend,
+				healthAndSafety: Number(review.healthandsafety),
+				managerRelationship: Number(review.managerrelationship),
+				workEnvironment: Number(review.workenvironment),
+				benefits: Number(review.benefits),
+				overallSatisfaction: Number(review.overallsatisfaction),
+				additionalComments: review.additionalcomments,
+				datePosted: review.dateadded,
+				upvotes: Number(reviewResults.votes[String(review.reviewid)].upvotes),
+				downvotes: Number(reviewResults.votes[String(review.reviewid)].downvotes)
+			};
+		});
+	}
+	return undefined;
 }
 
 let getSalaryById;
