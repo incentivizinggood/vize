744c3348a553b60fea07f2e1c41ffee499694e43
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-07-20 15:21:33 +0000

    Getting an idea of what publications and subscriptions I will need and how to go about replacing the current withTracker code, about to be able to start adding the new publications and replacing the old withTracker calls. However, there will also be a need to rewrite the parts of the model code to actually use the PostgreSQL helpers because the company search has already been migrated to GraphQL. Gotta make sure we hit that before too much longer.

--- imports/ui/pages/companyprofile.jsx
+++ imports/ui/pages/companyprofile.jsx
@@ -1,3 +1,4 @@
+import { Meteor } from "meteor/meteor";
 import React from "react";
 import { withTracker } from "meteor/react-meteor-data";
 import { Companies } from "../../api/data/companies.js";
