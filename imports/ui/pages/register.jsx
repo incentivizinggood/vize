1c600edbe2bfc0fc0bbb07d9eda07573af959c5f
joshuatshaffer <joshuatshaffer@gmail.com>
2018-08-07 21:07:02 -0500

    Fixed register page.

--- imports/ui/pages/register.jsx
+++ imports/ui/pages/register.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+
 import { Meteor } from "meteor/meteor";
 import { FlowRouter } from "meteor/kadira:flow-router";
 import { Accounts } from "meteor/accounts-base";
 import i18n from "meteor/universe:i18n";
-import Header from "../../ui/pages/header.jsx";
-import Footer from "../../ui/pages/footer.jsx";
-import Dialog from "./dialog-box.jsx"
-import 'font-awesome/css/font-awesome.min.css';
 
+import Header from "/imports/ui/components/header.jsx";
+import Footer from "/imports/ui/components/footer.jsx";
+import Dialog from "/imports/ui/components/dialog-box.jsx";
 
 const t = i18n.createTranslator("common.register");
 const T = i18n.createComponent(t);
@@ -21,6 +22,8 @@ export default class RegisterPage extends React.Component {
 			error: Meteor.userId() === null ? null : "loggedIn",
 			success: false,
 			username: "",
+			email: "",
+			companyName: "",
 			password: "",
 			role: "",
 		};
@@ -42,22 +45,19 @@ export default class RegisterPage extends React.Component {
 	}
 
 	handleInputChange(event) {
-		const {target} = event;
+		const { target } = event;
 		const value =
 			target.type === "checkbox" ? target.checked : target.value;
-		const {name} = target;
+		const { name } = target;
 
 		this.setState({
 			[name]: value,
 		});
 	}
 
-
 	handleSubmit(event) {
 		event.preventDefault(); // Prevent the default behavior for this event
-		alert("duh")
 		const createUserCallback = error => {
-
 			if (error) console.error(error);
 			this.setState({
 				// Using slice here to remove the period from the error reason
@@ -77,138 +77,207 @@ export default class RegisterPage extends React.Component {
 		Accounts.createUser(options, createUserCallback);
 	}
 
-	buttonClicked(event) {
-		event.preventDefault();
-		$(document).ready(function() {
-			$('#register-form-link').click(function(e) {
-				$("#register-form").delay(100).fadeIn(100);
-				$("#worker-form").fadeOut(100);
-				$('#worker-form-link').removeClass('active');
-				$(this).addClass('active');
-				e.preventDefault();
-			});
-
-			$('#worker-form-link').click(function(e) {
-				$("#worker-form").delay(100).fadeIn(100);
-				$("#register-form").fadeOut(100);
-				$('#register-form-link').removeClass('active');
-				$(this).addClass('active');
-				e.preventDefault();
-			});
-		});
-	}
-
 	render() {
 		if (this.state.success) {
 			return <div className="page register">Sign up successful!</div>;
 		}
-		return (
 
+		const roleInput = (
+			<>
+				<label
+					className={
+						this.state.role === "worker"
+							? "role selected-role"
+							: "role"
+					}
+					htmlFor="registerform-role-worker"
+				>
+					<input
+						id="registerform-role-worker"
+						name="role"
+						type="radio"
+						required
+						value="worker"
+						checked={this.state.role === "worker"}
+						onChange={this.handleInputChange}
+					/>
+					Employee
+				</label>
+				<label
+					className={
+						this.state.role === "company"
+							? "role selected-role"
+							: "role"
+					}
+					htmlFor="registerform-role-company"
+				>
+					<input
+						id="registerform-role-company"
+						name="role"
+						type="radio"
+						required
+						value="company"
+						checked={this.state.role === "company"}
+						onChange={this.handleInputChange}
+					/>
+					Employer
+				</label>
+			</>
+		);
+
+		const usernameInput = (
+			<div className="form-group">
+				<div className="icon-addon addon-md">
+					<input
+						type="text"
+						placeholder="Username"
+						className="form-control"
+						name="username"
+						id="username"
+						autoFocus
+						required
+						value={this.state.username}
+						onChange={this.handleInputChange}
+					/>
+					<label
+						htmlFor="Username"
+						className="glyphicon glyphicon-user"
+						rel="tooltip"
+						title="Username"
+					/>
+				</div>
+			</div>
+		);
+
+		const emailInput = (
+			<div className="form-group">
+				<div className="icon-addon addon-md">
+					<input
+						type="email"
+						name="email"
+						id="email"
+						className="form-control"
+						placeholder="Email"
+						value={this.state.email}
+						onChange={this.handleInputChange}
+					/>
+					<label
+						htmlFor="email"
+						className="fa fa-envelope-o"
+						rel="tooltip"
+						title="email"
+					/>
+				</div>
+			</div>
+		);
+
+		const companyNameInput = (
+			<div className="form-group">
+				<div className="icon-addon addon-md">
+					<input
+						type="text"
+						name="companyName"
+						id="companyName"
+						className="form-control"
+						placeholder="Company Name"
+						value={this.state.companyName}
+						onChange={this.handleInputChange}
+					/>
+					<label
+						htmlFor="companyName"
+						className="fa fa-building-o"
+						rel="tooltip"
+						title="companyName"
+					/>
+				</div>
+			</div>
+		);
+
+		const passwordInput = (
+			<div className="form-group">
+				<div className="icon-addon addon-md   pwd-line-sm">
+					<input
+						type="password"
+						name="password"
+						id="password"
+						className="form-control"
+						placeholder="Password"
+						required
+						value={this.state.password}
+						onChange={this.handleInputChange}
+					/>
+					<label
+						htmlFor="password"
+						className="fa fa-lock"
+						rel="tooltip"
+						title="password"
+					/>
+				</div>
+			</div>
+		);
+
+		const registerForm = (
+			<form
+				id="register-form"
+				style={{ display: "block" }}
+				onSubmit={this.handleSubmit}
+			>
+				<h3 className="top-head-employer" align="center">
+					Register
+				</h3>
+				{roleInput}
+				<br />
+
+				{this.state.role === "worker" ||
+				this.state.role === "company" ? (
+					<div className="employer-fm">
+						{usernameInput}
+
+						{emailInput}
+
+						{this.state.role === "company"
+							? companyNameInput
+							: null}
+
+						{passwordInput}
+
+						<div className="button-center">
+							<button
+								form="register-form"
+								type="submit"
+								className="button out-bodr-get1"
+							>
+								Get Started
+							</button>
+						</div>
+					</div>
+				) : null}
+			</form>
+		);
+
+		return (
 			<div className="page register">
 				{this.state.error ? <div>{this.state.error}</div> : null}
-				<Header/>
-				<div className="container  login-top-spce">
+				<Header />
+				<div className="container login-top-spce">
 					<div className="row">
 						<div className="col-md-6 col-md-offset-3">
-							<div className="panel panel-login   register-work-employee">
+							<div className="panel panel-login register-work-employee">
 								<div className="panel-body">
 									<div className="row">
 										<div className="col-lg-12">
-											<form id="register-form" action="" method="post" style= {{display: "none"}} onSubmit={this.handleSubmit}>
-												<h3 className="top-head-employer" align="center">Employer Register</h3>
-												<br/>
-												<div  className="employer-fm">
-													<div className="form-group">
-														<div className="icon-addon addon-md">
-															<input type="text" placeholder="Username" className="form-control"  name="username" id="username" autoFocus
-																   required
-																   value={this.state.username}
-																   onChange={this.handleInputChange}/>
-															<label for="Username" className="glyphicon glyphicon-user" rel="tooltip" title="Username" />
-														</div>
-													</div>
-
-													<div className="form-group">
-														<div className="icon-addon addon-md">
-															<input type="email" name="email" id="email" className="form-control"  placeholder="Email"/>
-															<label for="email" className="fa fa-envelope-o" rel="tooltip" title="email" />
-														</div>
-													</div>
-
-													<div className="form-group">
-														<div className="icon-addon addon-md">
-															<input type="text" name="companyName" id="companyName" className="form-control"  placeholder="Company Name"/>
-															<label for="companyName" className="fa fa-building-o" rel="tooltip" title="companyName"/>
-														</div>
-													</div>
-
-													<div className="form-group">
-														<div className="icon-addon addon-md   pwd-line-sm">
-															<input type="password" name="password" id="password" className="form-control"  placeholder="Password" required
-																   value={this.state.password}
-																   onChange={this.handleInputChange}/>
-															<label for="password" className="fa fa-lock" rel="tooltip" title="password"/>
-														</div>
-													</div>
-
-													<div className="button-center">
-														<button form="register-form" type="submit" className="button out-bodr-get1">Get Started</button>
-													</div>
-												</div>
-											</form>
-											<form id="worker-form" action="" method="post" role="form"   style={{display: "block"}}>
-												<h3  className="top-head-worker" align="center" >Employee  Register</h3>
-												<br/>
-												<div  className="employer-fm">
-													<div className="form-group">
-														<div className="icon-addon addon-md">
-															<input type="text" placeholder="Username" className="form-control  "  name="username" id="username" autoFocus
-																   required
-																   value={this.state.username}
-																   onChange={this.handleInputChange}/>
-															<label for="Username" className="glyphicon glyphicon-user" rel="tooltip" title="Username" />
-														</div>
-													</div>
-
-													<div className="form-group">
-														<div className="icon-addon addon-md">
-															<input type="email" name="email" id="email" className="form-control  "  placeholder="Email" />
-															<label for="email" className="fa fa-envelope-o" rel="tooltip" title="email"/>
-														</div>
-													</div>
-
-													<div className="form-group">
-														<div className="icon-addon addon-md   pwd-line-sm">
-															<input type="password" name="password" id="password" className="form-control  "  placeholder="Password" required
-																   value={this.state.password}
-																   onChange={this.handleInputChange}/>
-															<label for="password" className="fa fa-lock" rel="tooltip" title="password" />
-														</div>
-													</div>
-												</div>
-
-												<div className="button-center">
-													<button form="worker-form" type="submit" className="button out-bodr-get1">Get Started</button>
-												</div>
-											</form>
+											{registerForm}
 										</div>
 									</div>
 								</div>
-								<div className="panel-heading  p-head">
+								<div className="panel-heading p-head">
 									<div className="row">
 										<div className="col-lg-12">
-											<div className="text-center   login-link-cs">
-												Already have an account?  <a href="/login" > Log In </a>
+											<div className="text-center login-link-cs">
+												Already have an account?{" "}
+												<a href="/login"> Log In </a>
 											</div>
 											<div className="clearfix" />
-										</div>
-										<div className="col-xs-12 ">
-											<div className="btn-emp">
-												<a href="#"   className="button" id="register-form-link"  role="button" onClick={this.buttonClicked} >Employer</a>
-											</div>
-											<div  className="btn-work">
-												<a href="#" className="button  active" id="worker-form-link"  role="button" onClick={this.buttonClicked} >Employee</a>
-											</div>
 										</div>
 									</div>
 								</div>
@@ -216,10 +285,9 @@ export default class RegisterPage extends React.Component {
 						</div>
 					</div>
 				</div>
-				<Footer/>
-				<Dialog/>
+				<Footer />
+				<Dialog />
 			</div>
 		);
 	}
 }
-
