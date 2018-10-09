40e11717359aa176464d07b7743153614861b7f2
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-10-09 18:38:06 -0500

    More brainstorming, I think I've hashed out a to-do list that I should be able to start writing and testing with.

--- meteor-app/imports/ui/vize-formik/vize-formik-hoc.jsx
+++ meteor-app/imports/ui/vize-formik/vize-formik-hoc.jsx
@@ -43,6 +43,31 @@ import i18n from "meteor/universe:i18n";
 
 	WARNING
 	Don't be fooled: I don't actually know what I'm doing.
+
+	NOTE
+	Here are the "custom types" I will need to "support":
+	- star ratings
+	- locations (which may used nested custom inputs *shudder*)
+	- integers
+	- floats
+	- strings
+	- booleans
+	- arrays (of objects)
+
+	NOTE
+	And here are some other features that I will need to
+	figure out where and how they fit in:
+	- allowed values
+	- ...translation of allowed values
+	- company name drop-downs
+	- *custom validators involving Meteor Methods*
+	- optional/required fields
+	- max length check for strings
+	- min/max value check ("range check") for numbers
+	- minimum number of words (pros/cons)
+	- regex validation
+	- date fields (if necessary)
+	- hidden fields (if necessary)
 */
 
 const t = i18n.createTranslator();
