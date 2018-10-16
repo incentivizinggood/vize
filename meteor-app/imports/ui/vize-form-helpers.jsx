c2d5a916d91e635b93715f134f7a5ebadf28dd46
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-10-16 16:38:49 -0500

    ?

--- meteor-app/imports/ui/vize-form-helpers.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { VfInputText, VfInputTextWithOptionList } from "/imports/ui/components/vize-formik-components.jsx";
-
-import i18n from "meteor/universe:i18n";
-
-const t = i18n.createTranslator();
-const T = i18n.createComponent(t);
