c8d2e012e54a24246ab87f02a23aabf6a87ce315
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-10-17 13:28:03 -0500

    Comments and brainstorming

--- meteor-app/imports/ui/components/vize-formik-components.jsx
+++ meteor-app/imports/ui/components/vize-formik-components.jsx
@@ -10,31 +10,39 @@ import { Query } from "react-apollo";
 	Don't be fooled: I don't actually know what I'm doing.
 
 	NOTE
-	Here are the "custom types" I will need to "support":
+	Here are the remaining "custom types" I still need to "implement":
 	- star ratings
-	- locations (which may used nested custom inputs *shudder*)
 	- integers
 	- floats
-	- strings
 	- booleans
-	- arrays (of objects)
+	- arrays (of objects, perhaps of other custom types)
+
+	NOTE
+	Here are the types I have already implemented:
+	- locations (VfInputLocation)
+	- strings (VfInputText)
+	- string with externally-passed drop-down options (VfInputTextWithOptionList)
+	- text areas (VfInputTextArea)
 
 	NOTE
 	And here are some other features that I will need to
 	figure out where and how they fit in:
-	- allowed values
-	- ...translation of allowed values
-	- company name drop-downs
+	- translation of allowed values
 	- *custom validators involving Meteor Methods*
-	- optional/required fields
-	- max length check for strings
-	- min/max value check ("range check") for numbers
-	- minimum number of words (pros/cons)
-	- regex validation
 	- date fields (if necessary)
-	- hidden fields (if necessary)
 
-	QUESTION:
+	NOTE
+	Here are features that I have already hashed-out how to handle:
+	- allowed values (yup.mixed.oneOf)
+	- company name drop-downs (emptyCompanyNameField)
+	- max length check for strings (yup.mixed.max)
+	- regex validation (yup.string.matches)
+	- min/max value check ("range check") for numbers (yup.number.min/max)
+	- minimum number of words for pros/cons (yup.mixed.test(...(checks wordCount >= 5)))
+	- optional/required fields (yup.mixed.required/notRequired)
+	- hidden fields (if necessary) (included in Yup schema but not form, then initialized either in mapPropsToValues or handleSubmit)
+
+	QUESTION (NOTE: this question is old, see below for answer):
 		Given the above validation requirements, what are
 	my current options for validation with Formik?
 	#1 Manually call SimpleSchema.validate for every field,
@@ -63,6 +71,28 @@ import { Query } from "react-apollo";
 		and GraphQL validation functions only require a very
 		simple rewrite of a few lines, but I need to do some
 		more homework on this.
+
+	NOTE
+		I have chosen to go whole-ham with Yup/validationSchema.
+	This will allow us to enormously reduce the project's dependencies
+	on SimpleSchema and thereby on Tracker, as well as force a switch
+	to a more purely React-reliant style of programming, which will
+	make use of GraphQL for data requirements more than Meteor methods,
+	and thereby allow us to eliminate the Meteor methods that had
+	previously been used so heavily by SimpleSchema. In fact, it even
+	lays the groundword for removing universe:i18n, which is (currently)
+	tightly-coupled with SimpleSchema on account of field labels and
+	error messages, but will probably soon be used only for direct translation.
+		All that will remain is to hunt down the places where SimpleSchema
+	is used and replace it with Yup, and hunt down the places where
+	universe:i18n is used automagically and replace it with direct translation,
+	before removing universe:i18n can be replaced with an NPM i18n
+	package via a simple refactoring process.
+		Althought it is interesting to note that replacing universe:i18n
+	will require refactoring further up the stack beyond just copy-pasting
+	in new translation function calls, since code has had to be written
+	and rewritten around universe:i18n's limitations, especially in this
+	file.
 */
 
 const t = i18n.createTranslator();
@@ -76,6 +106,9 @@ const t = i18n.createTranslator();
 		might involve rethinking the architecture of this part of
 		the component hierarchy so as to bring in whatever contextual
 		information that is needed.
+		NOTE/ANSWER:
+			I did this by using props/connect "judiciously", and by
+			not using SimpleSchema. :)
 	QUESTION:
 		How do we stop the form from submitting when we change the
 		locale or otherwise re-render the component?
@@ -91,9 +124,15 @@ const t = i18n.createTranslator();
 		validation context properly? For that matter, how are we going
 		to set up that validation context, since it's not being done
 		automagically any longer?
+		NOTE/ANSWER:
+			This is easy enough with Yup/validationSchema, and can be
+			easily checked by running the form on the website.
 	QUESTION:
 		How will manually-calling SimpleSchema's validators affect
 		the reactivity context assumed by the custom validators?
+		NOTE/UPDATE: We are not using SimpleSchema. How re-implementing
+			the custom validators from within yup.mixed.test
+			affects reactivity remains to be seen.
 
 */
 
