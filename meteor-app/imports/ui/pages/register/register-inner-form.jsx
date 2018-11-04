d7154b495355815834709c8e1828fb23e2e31ebf
joshuatshaffer <joshuatshaffer@gmail.com>
2018-11-04 13:15:15 -0600

    Refactored out a form group component.

--- meteor-app/imports/ui/pages/register/register-inner-form.jsx
+++ meteor-app/imports/ui/pages/register/register-inner-form.jsx
@@ -1,97 +1,16 @@
 import React from "react";
-import { Field, Form } from "formik";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form } from "formik";
 
 import i18n from "meteor/universe:i18n";
 
 import withUpdateOnChangeLocale from "/imports/ui/hoc/update-on-change-locale.jsx";
+
 import IfFormik from "./if-formik.jsx";
 import RoleInput from "./role-input.jsx";
+import FormGroup from "./form-group.jsx";
 
 const t = i18n.createTranslator("common.loginRegister");
 const T = i18n.createComponent(t);
-
-const UsernameInput = () => (
-	<div className="form-group">
-		<label
-			htmlFor="Username"
-			className="icon-addon addon-md"
-			rel="tooltip"
-			title="Username"
-		>
-			<FontAwesomeIcon icon="user" className="fa" />
-			<Field
-				type="text"
-				placeholder={t("username")}
-				className="form-control"
-				name="username"
-				id="username"
-				required
-			/>
-		</label>
-	</div>
-);
-
-const EmailInput = () => (
-	<div className="form-group">
-		<label
-			htmlFor="email"
-			className="icon-addon addon-md"
-			rel="tooltip"
-			title="email"
-		>
-			<FontAwesomeIcon icon="envelope" className="fa" />
-			<Field
-				type="email"
-				name="email"
-				id="email"
-				className="form-control"
-				placeholder={t("email")}
-			/>
-		</label>
-	</div>
-);
-
-const CompanyNameInput = () => (
-	<div className="form-group">
-		<label
-			htmlFor="companyName"
-			className="icon-addon addon-md"
-			rel="tooltip"
-			title="companyName"
-		>
-			<FontAwesomeIcon icon="building" className="fa" />
-			<Field
-				type="text"
-				name="companyName"
-				id="companyName"
-				className="form-control"
-				placeholder={t("companyName")}
-			/>
-		</label>
-	</div>
-);
-
-const PasswordInput = () => (
-	<div className="form-group">
-		<label
-			htmlFor="password"
-			className="icon-addon addon-md pwd-line-sm"
-			rel="tooltip"
-			title="password"
-		>
-			<FontAwesomeIcon icon="lock" className="fa" />
-			<Field
-				type="password"
-				name="password"
-				id="password"
-				className="form-control"
-				placeholder={t("password")}
-				required
-			/>
-		</label>
-	</div>
-);
 
 function InnerForm({ errors, isSubmitting }) {
 	return (
@@ -109,15 +28,19 @@ function InnerForm({ errors, isSubmitting }) {
 				}
 			>
 				<div className="employer-fm">
-					<UsernameInput />
+					<FormGroup name="username" type="text" icon="user" />
 
-					<EmailInput />
+					<FormGroup name="email" type="email" icon="envelope" />
 
 					<IfFormik cond={formik => formik.values.role === "company"}>
-						<CompanyNameInput />
+						<FormGroup
+							name="companyName"
+							type="text"
+							icon="building"
+						/>
 					</IfFormik>
 
-					<PasswordInput />
+					<FormGroup name="password" type="password" icon="lock" />
 
 					<div className="button-center">
 						<button
