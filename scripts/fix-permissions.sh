09dc440dfa59bb0fb8f9d87a7bf9a6d39f9aa744
joshuatshaffer <joshuatshaffer@gmail.com>
2018-11-06 11:34:05 -0600

    Added comments to script.

--- scripts/fix-permissions.sh
+++ scripts/fix-permissions.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-git ls-tree --full-tree -r HEAD --name-only | grep -v '.sh$' | xargs chmod 644
-git ls-tree --full-tree -r HEAD --name-only | grep '.sh$' | xargs chmod 755
+# A grep pattern that matches only files which need to be executable.
+EX_PAT='*.sh$'
+
+# Set the modes of all non-executable files to 644
+# 644 = Everone can read, but only the owner can write.
+git ls-tree --full-tree -r HEAD --name-only | grep -v $EX_PAT | xargs chmod 644
+# Set the modes of all executable files to 755.
+# 755 = Everone can read and execute, but only the owner can write.
+git ls-tree --full-tree -r HEAD --name-only | grep $EX_PAT | xargs chmod 755
