ca1abc3cf201a61694b5a1d123715b5b60ee732f
joshuatshaffer <joshuatshaffer@gmail.com>
2018-11-08 07:37:47 -0600

    Fixed file modes.

--- scripts/fix-file-modes.sh
+++ scripts/fix-file-modes.sh
@@ -5,7 +5,7 @@ EX_PAT='*.sh$'
 
 # Set the modes of all non-executable files to 644
 # 644 = Everone can read, but only the owner can write.
-git ls-tree --full-tree -r HEAD --name-only | grep -v $EX_PAT | xargs chmod 644
+git ls-tree --full-tree -r HEAD --name-only | egrep -v $EX_PAT | xargs chmod 644
 # Set the modes of all executable files to 755.
 # 755 = Everone can read and execute, but only the owner can write.
-git ls-tree --full-tree -r HEAD --name-only | grep $EX_PAT | xargs chmod 755
+git ls-tree --full-tree -r HEAD --name-only | egrep $EX_PAT | xargs chmod 755
