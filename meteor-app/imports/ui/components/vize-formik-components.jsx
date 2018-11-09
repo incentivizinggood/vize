9c87cf6a7dabec3be25de28c25a9182269c7a43c
Joshua Higginbotham <jhigginbotham64@gmail.com>
2018-11-09 14:30:16 -0600

    Fixed a bug, got the error messages to show up on the location subfields. Now to see what additional complexities there are...

--- meteor-app/imports/ui/components/vize-formik-components.jsx
+++ meteor-app/imports/ui/components/vize-formik-components.jsx
@@ -182,7 +182,7 @@ const t = i18n.createTranslator();
 
 function getDescendantProp(obj, desc) {
 	let arr = desc.split('.');
-	while (arr.length) {
+	while (obj && arr.length) {
 		obj = obj[arr.shift()];
 	}
 	return obj;
@@ -190,7 +190,7 @@ function getDescendantProp(obj, desc) {
 
 function setDescendantProp(obj, desc, value) {
 	var arr = desc.split('.');
-	while (arr.length > 1) {
+	while (obj && arr.length > 1) {
 		obj = obj[arr.shift()];
 	}
 	return obj[arr[0]] = value;
