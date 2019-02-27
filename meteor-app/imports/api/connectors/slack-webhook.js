8087ee87a74f8b58a3504fc2eb64735895d4b447
joshuatshaffer <joshuatshaffer@gmail.com>
2019-02-27 17:31:19 -0600

    Cleaned and commented the Slack Webhook connector.

--- meteor-app/imports/api/connectors/slack-webhook.js
+++ meteor-app/imports/api/connectors/slack-webhook.js
@@ -1,21 +1,22 @@
-// @flow
 import request from "request-promise-native";
 
-opaque type MessageDecoration = string;
-
-export const celebrate: MessageDecoration = ":tada:";
-export const inform: MessageDecoration = ":information_source:";
-export const warn: MessageDecoration = ":warning:";
-export const reportError: MessageDecoration = ":skull_and_crossbones:";
-
-// TODO: Escape inputs to prevent markdown code injection.
-export function postToSlack(text: string) {
+/**
+ * Post a message to the Vize Slack.
+ * @param  {string} text The text of the message.
+ *                       Supports markdown and Slack's emoji markup.
+ * @todo Escape inputs to prevent markdown code injection.
+ */
+export function postToSlack(text) {
+	// Make a JSON representation of the message we want to post.
+	// This var is called "body" because it is the body of the HTTP request.
 	const body = {
 		text,
+		// Make this message look like it was posted by this server.
 		username: `Server at ${process.env.ROOT_URL}`,
 		icon_emoji: ":server:",
 	};
 
+	// Post the message to Vize's Slack's WebHook API.
 	const options = {
 		method: "POST",
 		uri: process.env.SLACK_WEBHOOK_URL,
@@ -23,5 +24,14 @@ export function postToSlack(text: string) {
 		json: true,
 	};
 
-	return request(options);
+	// Do not actualy make the request if the URL for Slack's WebHook API has not been set.
+	if (process.env.SLACK_WEBHOOK_URL) {
+		request(options);
+	} else {
+		console.warn(
+			"Could not post to slack because SLACK_WEBHOOK_URL was not set.",
+			"The message you tried to send was",
+			text
+		);
+	}
 }
