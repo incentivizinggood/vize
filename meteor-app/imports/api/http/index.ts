4771ee6d7a1e535616b145a7594cfeab2a7bda59
joshuatshaffer <joshuatshaffer@gmail.com>
2019-03-16 16:18:21 -0500

    Put middleware code into separet files for each middleware.

--- meteor-app/imports/api/http/index.ts
+++ meteor-app/imports/api/http/index.ts
@@ -1,5 +1,1 @@
-// Import all of the collection and method definitions.
-// These files need to be run to initialize the MongoDB connections.
-import "imports/api/data";
-
 export * from "./server";
--- meteor-app/imports/api/http/server.ts
+++ meteor-app/imports/api/http/server.ts
@@ -1,51 +1,14 @@
 import * as express from "express";
-import { ApolloServer } from "apollo-server-express";
 
-import { getUser } from "meteor/apollo";
-
-import { typeDefs, resolvers } from "imports/api/graphql";
 import { testConnection as testPgConnection } from "imports/api/connectors/postgresql-old";
+
+import { applyGraphQLMiddleware } from "./graphql-middleware";
+import { applyHelloWorldMiddleware } from "./hello-world-middleware";
 
 const app = express();
 
-// A hello world for testing the Express server.
-app.get("/express-test", function(_req, res) {
-	res.send(
-		`<!DOCTYPE html>
-<html>
-	<head>
-		<title>Hello from Express</title>
-	</head>
-	<body>
-		<h1>Hello world!</h1>
-		<p>The Express server seems to be working.</p>
-	</body>
-</html>
-`
-	);
-});
-
-// Setup the GraphQL API endpoint.
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	context: async ({ req }) => ({
-		// Get the user using a token in the headers. Will be changed when
-		// switching away from Meteor's authorization framework.
-		user: await getUser(req.headers.authorization),
-	}),
-});
-
-server.applyMiddleware({
-	app,
-	path: "/graphql",
-});
-
-app.use("/graphql", (req, res) => {
-	if (req.method === "GET") {
-		res.end();
-	}
-});
+applyHelloWorldMiddleware(app);
+applyGraphQLMiddleware(app);
 
 // TODO: When we stop using Meteor, `app` will be the main server.
 //       We will need to call app.listen to start the server.
