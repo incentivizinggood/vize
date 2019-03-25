a5007ba823dfd0b8e30630c562731833fa4300ef
joshuatshaffer <joshuatshaffer@gmail.com>
2019-03-25 18:42:45 -0500

    Fixed vote buttons working with null votes.

--- meteor-app/imports/ui/components/vote-buttons/vote-button.jsx
+++ meteor-app/imports/ui/components/vote-buttons/vote-button.jsx
@@ -3,29 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import style from "./vote-buttons.scss";
 
-export default function VoteButton({ isUpButton, castVote, review }) {
+function voteToCast(vote, isUpButton) {
+	if (vote === null || vote.isUpvote === null) {
+		return isUpButton;
+	} else if (vote.isUpvote === isUpButton) {
+		return null;
+	} else {
+		return isUpButton;
+	}
+}
+
+export default function VoteButton({ isUpButton = false, castVote, review }) {
+	const onClick = event => {
+		event.preventDefault();
+
+		castVote({
+			variables: {
+				input: {
+					isUpvote: voteToCast(review.currentUserVote, isUpButton),
+					subjectId: review.id,
+				},
+			},
+		});
+	};
+
 	return (
 		<button
 			type="button"
 			className={isUpButton ? style.upButton : style.downButton}
-			onClick={event => {
-				console.log(
-					`Clicked the ${isUpButton ? "up" : "down"} button.`
-				);
-				event.preventDefault();
-				castVote({
-					variables: {
-						input: {
-							isUpvote:
-								review.currentUserVote !== null &&
-								review.currentUserVote.isUpvote === isUpButton
-									? null
-									: isUpButton,
-							subjectId: review.id,
-						},
-					},
-				});
-			}}
+			onClick={onClick}
 		>
 			{isUpButton ? (
 				<FontAwesomeIcon icon="thumbs-up" />
--- meteor-app/imports/ui/components/vote-buttons/vote-buttons.jsx
+++ meteor-app/imports/ui/components/vote-buttons/vote-buttons.jsx
@@ -1,36 +1,35 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Mutation } from "react-apollo";
 
 import style from "./vote-buttons.scss";
 import voteButtonsQuery from "./vote-buttons.graphql";
 import VoteButton from "./vote-button.jsx";
 
-export default function VoteButtons(props) {
-	const { review, refetch, className, ...otherProps } = props;
-
-	let curVote;
-	if (review.currentUserVote) {
-		curVote = review.currentUserVote.isUpvote ? "up" : "down";
+function voteToString(vote) {
+	if (vote === null || vote.isUpvote === null) {
+		return "none";
+	} else if (vote.isUpvote) {
+		return "up";
 	} else {
-		curVote = "none";
+		return "down";
 	}
+}
+
+export default function VoteButtons(props) {
+	const { review, className, ...otherProps } = props;
 
 	return (
 		<Mutation mutation={voteButtonsQuery}>
-			{(castVote, stuff) => {
-				console.log(stuff);
-				return (
-					<div
-						{...otherProps}
-						className={style.voteButtons + (className || "")}
-						data-vote={curVote}
-					>
-						<VoteButton isUpButton {...{ castVote, review }} />
-						<VoteButton {...{ castVote, review }} />
-					</div>
-				);
-			}}
+			{castVote => (
+				<div
+					{...otherProps}
+					className={style.voteButtons + (className || "")}
+					data-vote={voteToString(review.currentUserVote)}
+				>
+					<VoteButton isUpButton {...{ castVote, review }} />
+					<VoteButton {...{ castVote, review }} />
+				</div>
+			)}
 		</Mutation>
 	);
 }
