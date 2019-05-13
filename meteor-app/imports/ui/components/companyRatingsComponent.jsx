d08f909a76be17d4a20997d497c99922ad248bdb
krit <krit.nsit@gmail.com>
2019-05-13 18:30:16 -0500

    removed the ripple effect from the company profile

--- meteor-app/imports/ui/components/companyRatingsComponent.jsx
+++ meteor-app/imports/ui/components/companyRatingsComponent.jsx
@@ -91,6 +91,12 @@ ChangingProgressbar.defaultProps = {
 };
 
 function CompanyRating(props) {
+	// 	componentDidMount(){
+	//   //Do this to all components you would like to disable the ripple effect.
+	//   CompanyRating.defaultProps.disableTouchRipple = true;
+	//   CompanyRating.defaultProps.disableFocusRipple = true;
+	// }
+
 	// The number of digits to display after the decimal point.
 	const numDigits = 2;
 	const { classes } = props;
@@ -99,13 +105,23 @@ function CompanyRating(props) {
 		<div>
 			<div className="row">
 				<div className="col-md-6  bodr_lft">
-					<Card className={classes.card}>
-						<CardActionArea>
+					<Card
+						style={{ backgroundColor: "transparent" }}
+						disableFocusRipple
+						className={classes.card}
+					>
+						<CardActionArea
+							style={{ backgroundColor: "transparent" }}
+							disableTouchRipple
+							disableFocusRipple="true"
+						>
 							<CardMedia>
 								<Typography
 									gutterBottom
 									variant="h5"
 									component="h2"
+									disableFocusRipple
+									style={{ backgroundColor: "transparent" }}
 								>
 									<div className="star_border ">
 										<label>
@@ -327,211 +343,6 @@ function CompanyRating(props) {
 				<div className="clear" />
 			</div>
 		</div>
-
-		// <div>
-		// 	<div className="row">
-		// 		<div className="col-md-6  bodr_lft">
-		// 			<div className="star_border ">
-		// 				<label>
-		// 					<T>common.company_ratings.overall</T>
-		// 				</label>
-		// 				&nbsp;&nbsp;&nbsp;&nbsp;
-		// 				<StarRatings
-		// 					rating={
-		// 						props.company.avgStarRatings.overallSatisfaction
-		// 					}
-		// 					starDimension="20px"
-		// 					starSpacing="1.6px"
-		// 				/>
-		// 				&nbsp;&nbsp;&nbsp;&nbsp;
-		// 				<label>
-		// 					{props.company.avgStarRatings.overallSatisfaction.toFixed(
-		// 						numDigits
-		// 					)}
-		// 				</label>
-		// 			</div>
-		// 			<div className="tab_str">
-		// 				<table>
-		// 					<tbody>
-		// 						<tr>
-		// 							<td>
-		// 								<label
-		// 									htmlFor="input-2"
-		// 									className="control-label  lef_label"
-		// 								>
-		// 									<T>
-		// 										common.company_ratings.health_safety
-		// 									</T>
-		// 								</label>
-		// 							</td>
-		// 							<td>
-		// 								{/* star ratings are left */}
-		// 								<div className="row">
-		// 									<StarRatings
-		// 										rating={
-		// 											props.company.avgStarRatings
-		// 												.healthAndSafety
-		// 										}
-		// 										starDimension="20px"
-		// 										starSpacing="1.8px"
-		// 									/>
-		// 									&nbsp;&nbsp;{" "}
-		// 									<label>
-		// 										{props.company.avgStarRatings.healthAndSafety.toFixed(
-		// 											numDigits
-		// 										)}
-		// 									</label>
-		// 								</div>
-		// 							</td>
-		// 						</tr>
-		// 						<tr>
-		// 							<td>
-		// 								<label
-		// 									htmlFor="input-3"
-		// 									className="control-label lef_label"
-		// 								>
-		// 									<T>
-		// 										common.company_ratings.work_env
-		// 									</T>
-		// 								</label>
-		// 							</td>
-		// 							<td>
-		// 								<div className="row">
-		// 									<StarRatings
-		// 										rating={
-		// 											props.company.avgStarRatings
-		// 												.workEnvironment
-		// 										}
-		// 										starDimension="20px"
-		// 										starSpacing="1.8px"
-		// 									/>
-		// 									&nbsp;&nbsp;{" "}
-		// 									<label>
-		// 										{props.company.avgStarRatings.workEnvironment.toFixed(
-		// 											numDigits
-		// 										)}
-		// 									</label>
-		// 								</div>
-		// 							</td>
-		// 						</tr>
-		// 						<tr>
-		// 							<td>
-		// 								<label
-		// 									htmlFor="input-4"
-		// 									className="control-label   lef_label"
-		// 								>
-		// 									<T>
-		// 										common.company_ratings.benefits
-		// 									</T>
-		// 								</label>
-		// 							</td>
-		// 							<td>
-		// 								<div className="row">
-		// 									<StarRatings
-		// 										rating={
-		// 											props.company.avgStarRatings
-		// 												.benefits
-		// 										}
-		// 										starDimension="20px"
-		// 										starSpacing="1.8px"
-		// 									/>
-		// 									&nbsp;&nbsp;{" "}
-		// 									<label>
-		// 										{props.company.avgStarRatings.benefits.toFixed(
-		// 											numDigits
-		// 										)}
-		// 									</label>
-		// 								</div>
-		// 							</td>
-		// 						</tr>
-		// 						<tr>
-		// 							<td>
-		// 								{" "}
-		// 								<label
-		// 									htmlFor="input-5"
-		// 									className="control-label  lef_label"
-		// 								>
-		// 									<T>
-		// 										common.company_ratings.manager_relation
-		// 									</T>
-		// 								</label>
-		// 							</td>
-		// 							<td>
-		// 								<div className="row">
-		// 									<StarRatings
-		// 										rating={
-		// 											props.company.avgStarRatings
-		// 												.managerRelationship
-		// 										}
-		// 										starDimension="20px"
-		// 										starSpacing="1.8px"
-		// 									/>
-		// 									&nbsp;&nbsp;{" "}
-		// 									<label>
-		// 										{props.company.avgStarRatings.managerRelationship.toFixed(
-		// 											numDigits
-		// 										)}
-		// 									</label>
-		// 								</div>
-		// 							</td>
-		// 						</tr>
-		// 					</tbody>
-		// 				</table>
-		// 			</div>
-		// 		</div>
-		//
-		// 		<div className="col-md-6">
-		// 			<center>
-		// 				{" "}
-		// 				<h3>
-		// 					{" "}
-		// 					<T>common.company_ratings.recommend</T>{" "}
-		// 				</h3>
-		// 			</center>
-		//
-		// 			<br />
-		//
-		// 			<div
-		// 				style={{
-		// 					width: "50%",
-		// 					height: "50%",
-		// 					margin: "0 auto",
-		// 				}}
-		// 			>
-		// 				<ChangingProgressbar
-		// 					percentages={[
-		// 						Math.round(
-		// 							props.company.percentRecommended * 100
-		// 						),
-		// 					]}
-		// 					strokeWidth={10}
-		// 					initialAnimation
-		// 				/>
-		// 			</div>
-		// 			<center>
-		// 				<div className="col-md-6">
-		// 					<div className="num_sett">
-		// 						<h1>
-		// 							{props.company.avgNumMonthsWorked.toFixed(
-		// 								numDigits
-		// 							)}{" "}
-		// 						</h1>
-		// 					</div>
-		// 				</div>
-		// 				<div className="col-md-6">
-		// 					<div className="num_sett">
-		// 						{" "}
-		// 						<span>
-		// 							<T>common.company_ratings.average_num</T>{" "}
-		// 							<T>common.company_ratings.months_worked</T>
-		// 						</span>
-		// 					</div>
-		// 				</div>
-		// 			</center>
-		// 		</div>
-		// 	</div>
-		// 	<div className="clear" />
-		// </div>
 	);
 }
 
@@ -554,4 +365,4 @@ CompanyRating.defaultProps = {
 	},
 };
 
-export default withStyles(styles)(CompanyRating);
+export default withStyles(styles, { withTheme: true })(CompanyRating);
