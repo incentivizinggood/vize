0c3f6d0470e1287e0efec57b51ca544d0c1bfc89
joshuatshaffer <joshuatshaffer@gmail.com>
2019-05-13 18:11:21 -0500

    Disabled the report button when the user is not logged in.

--- meteor-app/imports/api/models/flags.ts
+++ meteor-app/imports/api/models/flags.ts
@@ -1,36 +1,37 @@
 import { sendEmail } from "imports/api/connectors/email";
 import sql from "imports/lib/sql-template";
 import { simpleQuery1 } from "imports/api/connectors/postgresql";
-import {getUserById, UserId} from ".";
+import { getUserById, UserId } from ".";
 
 export async function flagAReview(
 	reviewId: string,
-  userId: UserId,
+	userId: UserId,
 	reason: string,
 	explanation: string
-
 ) {
 	const reviewInfo = await simpleQuery1<{
 		reviewTitle: string;
 		companyName: string;
 	}>(
 		sql`
-      SELECT reviewtitle AS "reviewTitle", companies.name AS "companyName"
-      FROM reviews
-      JOIN companies ON reviews.companyid = companies.companyid
-      WHERE reviewid = ${reviewId}
-    `
+			SELECT reviewtitle AS "reviewTitle", companies.name AS "companyName"
+			FROM reviews
+			JOIN companies ON reviews.companyid = companies.companyid
+			WHERE reviewid = ${reviewId}
+		`
 	);
 	if (reviewInfo === null) {
 		throw new Error("That review does not exist.");
 	}
 
-  const user = await getUserById(userId);
+	const user = await getUserById(userId);
 
 	sendEmail({
 		to: "incentivizinggood@gmail.com",
 		subject: "Someone flagged a review",
-		text: `UserName: ${user.username}\nReason: ${reason}\nExplanation: ${explanation}\nReview Title: ${
+		text: `UserName: ${
+			user.username
+		}\nReason: ${reason}\nExplanation: ${explanation}\nReview Title: ${
 			reviewInfo.reviewTitle
 		}\nReview Id: ${reviewId}\nCompany Name: ${reviewInfo.companyName}`,
 	});
