b1cacb50076452eac672712fe99cfd9c25274ecc
joshuatshaffer <joshuatshaffer@gmail.com>
2019-05-19 16:19:28 -0500

    Moved static values outside of form components for better readability and preformance.

--- meteor-app/imports/ui/pages/login/login-form.js
+++ meteor-app/imports/ui/pages/login/login-form.js
@@ -6,66 +6,64 @@ import { Meteor } from "meteor/meteor";
 
 import InnerForm from "./login-inner-form.jsx";
 
+const initialValues = {
+	username: "",
+	password: "",
+};
+
+const validate = values => {
+	const errors = {};
+
+	if (!values.username) {
+		errors.username = "Required";
+	}
+
+	if (!values.password) {
+		errors.password = "Required";
+	}
+
+	return errors;
+};
+
+const onSubmit = history => (values, actions) => {
+	const loginCallback = error => {
+		if (error) {
+			console.error(error);
+
+			// Errors to display on form fields
+			const formErrors = {};
+
+			if (error.reason === "User not found") {
+				formErrors.username = "User not found";
+			}
+			if (error.reason === "Incorrect password") {
+				// TODO: clear the password input on this error
+				formErrors.password = "Incorrect password";
+			}
+
+			actions.setErrors(formErrors);
+			actions.setSubmitting(false);
+		} else {
+			actions.resetForm(initialValues);
+			if (
+				!(
+					window.location.pathname.includes("/write-review") ||
+					window.location.pathname.includes("/submit-salary-data")
+				)
+			) {
+				history.push("/");
+			}
+		}
+	};
+
+	Meteor.loginWithPassword(values.username, values.password, loginCallback);
+};
+
 const LoginForm = props => (
 	<Formik
-		initialValues={{
-			username: "",
-			password: "",
-		}}
-		validate={values => {
-			const errors = {};
-
-			if (!values.username) {
-				errors.username = "Required";
-			}
-
-			if (!values.password) {
-				errors.password = "Required";
-			}
-
-			return errors;
-		}}
-		onSubmit={(values, actions) => {
-			const loginCallback = error => {
-				if (error) {
-					console.error(error);
-
-					// Errors to display on form fields
-					const formErrors = {};
-
-					if (error.reason === "User not found") {
-						formErrors.username = "User not found";
-					}
-					if (error.reason === "Incorrect password") {
-						// TODO: clear the password input on this error
-						formErrors.password = "Incorrect password";
-					}
-
-					actions.setErrors(formErrors);
-					actions.setSubmitting(false);
-				} else {
-					actions.resetForm(this.initialValues);
-					if (
-						!(
-							window.location.pathname.includes(
-								"/write-review"
-							) ||
-							window.location.pathname.includes(
-								"/submit-salary-data"
-							)
-						)
-					) {
-						props.history.push("/");
-					}
-				}
-			};
-
-			Meteor.loginWithPassword(
-				values.username,
-				values.password,
-				loginCallback
-			);
-		}}
+		initialValues={initialValues}
+		validate={validate}
+		onSubmit={onSubmit(props.history)}
 	>
 		<InnerForm />
 	</Formik>
--- meteor-app/imports/ui/pages/register/register-form.js
+++ meteor-app/imports/ui/pages/register/register-form.js
@@ -8,72 +8,74 @@ import InnerForm from "./register-inner-form.jsx";
 
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const initialValues = {
+	username: "",
+	email: "",
+	companyName: "",
+	password: "",
+	role: "",
+};
+
+const validate = values => {
+	const errors = {};
+
+	if (!values.username) {
+		errors.username = "Required";
+	}
+
+	if (!values.password) {
+		errors.password = "Required";
+	}
+
+	if (values.email && !emailRegex.test(values.email)) {
+		errors.email = "Not a valid email";
+	}
+
+	return errors;
+};
+
+const onSubmit = history => (values, actions) => {
+	const createUserCallback = error => {
+		if (error) {
+			console.error(error);
+
+			// Errors to display on form fields
+			const formErrors = {};
+
+			if (error.reason === "Username already exists.") {
+				formErrors.username = "Username already exists";
+			}
+
+			actions.setErrors(formErrors);
+			actions.setSubmitting(false);
+		} else {
+			actions.resetForm(initialValues);
+			// checks to see if the current page is the write a reivew page.
+			// if the current page is write a review page and a register is successful
+			// there should be no redirect so that the user can stay on the write a review page
+			if (
+				!(
+					window.location.pathname.includes("/write-review") ||
+					window.location.pathname.includes("/submit-salary-data")
+				)
+			) {
+				history.push("/");
+			}
+		}
+	};
+	const options = {
+		username: values.username,
+		password: values.password,
+		role: values.role,
+	};
+	Accounts.createUser(options, createUserCallback);
+};
+
 const RegisterForm = props => (
 	<Formik
-		initialValues={{
-			username: "",
-			email: "",
-			companyName: "",
-			password: "",
-			role: "",
-		}}
-		validate={values => {
-			const errors = {};
-
-			if (!values.username) {
-				errors.username = "Required";
-			}
-
-			if (!values.password) {
-				errors.password = "Required";
-			}
-
-			if (values.email && !emailRegex.test(values.email)) {
-				errors.email = "Not a valid email";
-			}
-
-			return errors;
-		}}
-		onSubmit={(values, actions) => {
-			const createUserCallback = error => {
-				if (error) {
-					console.error(error);
-
-					// Errors to display on form fields
-					const formErrors = {};
-
-					if (error.reason === "Username already exists.") {
-						formErrors.username = "Username already exists";
-					}
-
-					actions.setErrors(formErrors);
-					actions.setSubmitting(false);
-				} else {
-					actions.resetForm(this.initialValues);
-					// checks to see if the current page is the write a reivew page.
-					// if the current page is write a review page and a register is successful
-					// there should be no redirect so that the user can stay on the write a review page
-					if (
-						!(
-							window.location.pathname.includes(
-								"/write-review"
-							) ||
-							window.location.pathname.includes(
-								"/submit-salary-data"
-							)
-						)
-					) {
-						props.history.push("/");
-					}
-				}
-			};
-			const options = {
-				username: values.username,
-				password: values.password,
-				role: values.role,
-			};
-			Accounts.createUser(options, createUserCallback);
-		}}
+		initialValues={initialValues}
+		validate={validate}
+		onSubmit={onSubmit(props.history)}
 	>
 		<InnerForm />
 	</Formik>
