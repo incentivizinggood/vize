4fb282a9ce0461d57116af08dad92e0fb38388d2
joshuatshaffer <joshuatshaffer@gmail.com>
2019-05-20 15:02:07 -0500

    Made a styled button component and a page to test them on.

---
+++ meteor-app/imports/ui/components/button/button.jsx
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+const vizeBlue = "#2699FB";
+const vizeDarkerBlue = "#1d80d6";
+
+const Button = styled.a`
+	&&&&&&&&& {
+		${props => {
+			if (props.disabled) {
+				if (props.primary) {
+					return css`
+						background-color: lightgrey;
+						color: white;
+					`;
+				}
+
+				return css`
+					background-color: white;
+					color: lightgrey;
+					border: 1px solid;
+				`;
+			}
+
+			if (props.primary) {
+				return css`
+					background-color: ${vizeBlue};
+					color: white;
+					:hover {
+						background-color: ${vizeDarkerBlue};
+					}
+				`;
+			}
+
+			return css`
+				background-color: white;
+				color: ${vizeBlue};
+				:hover {
+					color: ${vizeDarkerBlue};
+				}
+				border: 1px solid;
+			`;
+		}}
+
+		${props =>
+			props.disabled &&
+			css`
+				pointer-events: none;
+			`}
+
+		font-weight: bold;
+
+		display: inline-block;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		padding: 0.9rem 3rem;
+		line-height: 1.5;
+		border-radius: 0.25rem;
+		transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+			border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+		cursor: pointer;
+		text-decoration: none;
+	}
+`;
+
+export default Button;
---
+++ meteor-app/imports/ui/pages/test/test.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+import PageWrapper from "/imports/ui/components/page-wrapper";
+import Button from "/imports/ui/components/button";
+
+const TestGrid = styled.div`
+	display: grid;
+	grid-template-columns: auto auto;
+	grid-template-rows: 200px 200px;
+	justify-items: center;
+	align-items: center;
+`;
+
+function TestPage() {
+	return (
+		<PageWrapper>
+			<TestGrid>
+				<Button>Default</Button>
+				<Button primary>Primary</Button>
+				<Button disabled>Disabled</Button>
+				<Button disabled primary>
+					Disabled Primary
+				</Button>
+			</TestGrid>
+		</PageWrapper>
+	);
+}
+
+export default TestPage;
