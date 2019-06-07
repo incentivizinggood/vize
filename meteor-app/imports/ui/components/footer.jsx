ad1147e0e4446927adb314b711d1afc491ef87f9
joshuatshaffer <joshuatshaffer@gmail.com>
2019-06-07 09:28:30 -0500

    Componentized the footer's styles.

--- meteor-app/imports/ui/components/footer.jsx
+++ meteor-app/imports/ui/components/footer.jsx
@@ -1,13 +1,142 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import i18n from "meteor/universe:i18n";
 
+import { forSize } from "/imports/ui/responsive.js";
+import styleVariables from "/imports/ui/style-variables.js";
+
 const T = i18n.createComponent();
+
+const FooterContainer = styled.div`
+	/* From sass/base/_reset.scss and sass/pages/_company-profile.scss */
+	padding: 8em 0;
+
+	/* From sass/components/_footer.scss */
+	background: #232326;
+	padding-top: 5%;
+	padding-bottom: 0;
+	text-align: left;
+	color: #cbcbcb;
+
+	h3 {
+		font-size: 2.5em;
+		color: ${styleVariables.blueMarguerite};
+		margin-bottom: 0.5em;
+	}
+
+	a {
+		font-size: 17px;
+		color: #cbcbcb;
+
+		&:hover,
+		&:focus {
+			color: ${styleVariables.vizeBlue};
+		}
+	}
+
+	.footer-grids {
+		h4 {
+			color: #767676;
+			text-transform: uppercase;
+			margin-bottom: 19px;
+			font-size: 23px;
+		}
+
+		&:nth-child(1) {
+			padding: 0 5em 0 0;
+		}
+
+		span {
+			display: block;
+		}
+	}
+
+	.footer-copy {
+		margin: 3em 0 0;
+		padding: 2em 0;
+		border-top: 1px solid #aaa;
+
+		p {
+			text-align: center;
+		}
+	}
+
+	ul.footer_nav {
+		list-style-type: none;
+	}
+
+	ul.footer_nav {
+		.navigation4,
+		.navigation3,
+		.navigation2,
+		.navigation1 {
+			font-size: 17px;
+			list-style: none;
+			line-height: 31px;
+		}
+	}
+
+	/* From sass/responsive/_desktop-and-down.scss */
+	${forSize.desktopAndDown} {
+		padding: 4em 0;
+
+		.footer-bottom {
+			a {
+				margin: 0 0.5em;
+			}
+
+			span {
+				left: -20px;
+				right: -20px;
+				font-size: 0.9em;
+
+				&:before {
+					left: 33px;
+				}
+			}
+		}
+
+		.footer-grids {
+			float: left;
+			width: 33.33%;
+			padding: 0 0.5em;
+
+			&:nth-child(1) {
+				padding: 0 1em 0 0;
+			}
+
+			&:nth-child(2) {
+				margin: 1em 0;
+			}
+		}
+
+		img {
+			padding: 0;
+			width: 100%;
+		}
+	}
+
+	/* From sass/responsive/_tablet-landscape-and-down.scss */
+	${forSize.tabletLandscapeAndDown} {
+		padding: 5em 0;
+
+		.footer-grids {
+			padding-left: 0;
+			margin-top: 10px;
+		}
+	}
+
+	/* From sass/responsive/_phones-only.scss */
+	${forSize.phoneOnly} {
+		padding: 3em 0;
+	}
+`;
 
 export default function Footer() {
 	return (
-		<div className="footer">
+		<FooterContainer>
 			<div className="container">
 				<div className="col-md-3 footer-grids">
 					<h4>Vize</h4>
@@ -90,6 +219,6 @@ export default function Footer() {
 					</p>
 				</div>
 			</div>
-		</div>
+		</FooterContainer>
 	);
 }
