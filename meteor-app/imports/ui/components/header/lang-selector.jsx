e582bd7f8d2c5b8b46040c758f8e358b92c21ea6
joshuatshaffer <joshuatshaffer@gmail.com>
2019-06-07 16:46:30 -0500

    Converted the language selector to use reactjs-popup.

--- meteor-app/imports/ui/components/header/lang-selector.jsx
+++ meteor-app/imports/ui/components/header/lang-selector.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Dropdown, {
-	DropdownTrigger,
-	DropdownContent,
-} from "react-simple-dropdown";
+import Popup from "reactjs-popup";
 
 import { i18n } from "meteor/universe:i18n";
 import { withTracker } from "meteor/react-meteor-data";
@@ -22,36 +19,39 @@ const CurLangContainer = withTracker(() => ({
 	code: reactiveGetLocale(),
 }))(CurLang);
 
+const langOptions = close => (
+	<ul>
+		{Object.keys(localeMetadata).map(code => (
+			<li key={code}>
+				<button
+					onClick={() => {
+						i18n.setLocale(code);
+						close();
+					}}
+				>
+					<img
+						src={localeMetadata[code].icon}
+						alt={`${code} icon.`}
+					/>
+					<span>{localeMetadata[code].nativeName}</span>
+				</button>
+			</li>
+		))}
+	</ul>
+);
+
 function LangSelector() {
-	const refDropdown = React.useRef(null);
-
-	const hideDropdown = () => {
-		refDropdown.current.hide();
-	};
-
-	const LangOpt = code => (
-		<li key={code}>
-			<button
-				onClick={() => {
-					i18n.setLocale(code);
-					hideDropdown();
-				}}
-			>
-				<img src={localeMetadata[code].icon} alt={`${code} icon.`} />
-				<span>{localeMetadata[code].nativeName}</span>
-			</button>
-		</li>
-	);
-
 	return (
-		<Dropdown ref={refDropdown}>
-			<DropdownTrigger>
-				<CurLangContainer />
-			</DropdownTrigger>
-			<DropdownContent>
-				<ul>{Object.keys(localeMetadata).map(LangOpt)}</ul>
-			</DropdownContent>
-		</Dropdown>
+		<Popup
+			trigger={
+				<button className="button">
+					<CurLangContainer />
+				</button>
+			}
+			closeOnDocumentClick
+		>
+			{langOptions}
+		</Popup>
 	);
 }
 
