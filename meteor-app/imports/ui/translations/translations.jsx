421fc0b488782ace97e7e236934ea9df0a3dab6f
joshuatshaffer <joshuatshaffer@gmail.com>
2019-06-29 11:53:55 -0500

    Refactored the translation system.

--- meteor-app/imports/ui/translations/translations.jsx
+++ meteor-app/imports/ui/translations/translations.jsx
@@ -6,62 +6,64 @@ import withUpdateOnChangeLocale from "/imports/ui/hoc/update-on-change-locale.js
 import { getAtPath, joinPaths } from "/imports/lib/property-path.ts";
 
 /**
- * A component that renders different thing for each locale.
- * @param translations A record with one element for each locale that we support.
- * @param restProps    If the translations are functions, they will be called with the other props.
+ * Made translation components for each message in a collection of translations.
  */
-const I18nSwitch = withUpdateOnChangeLocale(
-	({ translations, messageKey, renderer, args }) => {
-		const locale = i18n.getLocale();
-
-		const translation = getAtPath(translations[locale], messageKey);
-
-		const message =
-			typeof translation === "function" ? translation(args) : translation;
-
-		return renderer !== undefined ? renderer(message) : message;
-	}
-);
-
-function minka(translations, defaultTranslation, messageKey) {
-	console.log("minka", translations, defaultTranslation, messageKey);
-
-	function kalbo({ renderer, ...args }) {
-		return (
-			<I18nSwitch
-				renderer={renderer}
-				args={args}
-				translations={translations}
-				messageKey={messageKey}
-			/>
-		);
-	}
-
-	// If the message is an object but not a React element (i.e. a group of
-	// other messages), we recursivly make translation compoents for all of its
-	// enteries.
-	const message = getAtPath(defaultTranslation, messageKey);
-	if (
-		message != null &&
-		typeof message === "object" &&
-		!React.isValidElement(message)
-	) {
-		Object.keys(message).forEach(k => {
-			kalbo[k] = minka(
-				translations,
-				defaultTranslation,
-				joinPaths(messageKey, k)
-			);
-		});
-	}
-
-	return kalbo;
-}
-
 function makeTranslationComponents(translations) {
+	// Pick a locale to use as the "default translations". This is only used for
+	// computing the message paths and has no effect on how what is used as a
+	// default locale in other places.
 	const defaultTranslation = Object.values(translations)[0];
 
-	return minka(translations, defaultTranslation, "");
+	function makeTranslationComponent(messagePath) {
+		// A component that renders a different thing for each locale.
+		const TranslationComponent = withUpdateOnChangeLocale(
+			({ renderer, ...args }) => {
+				// Get the version of the message that is for the current locale.
+				const locale = i18n.getLocale();
+				let message = getAtPath(translations[locale], messagePath);
+
+				// Messages can be functions of the translation component's props.
+				if (typeof message === "function") {
+					message = message(args);
+				}
+
+				// Translation components may be given a custom renderer to use.
+				// This is mainly ment for where some props of a component need
+				// to be translated but not the whole component.
+				if (typeof renderer === "function") {
+					return renderer(message);
+				} else if (renderer instanceof React.Component) {
+					return React.createElement(renderer, message);
+				} else if (renderer !== undefined) {
+					console.error(
+						"The renderer prop of a translation component must be a function or a React.Component."
+					);
+				}
+
+				return message;
+			}
+		);
+
+		// If the message is an object but not a React element (i.e. a group of
+		// other messages), we recursivly make translation components for all of
+		// its properties.
+		const message = getAtPath(defaultTranslation, messagePath);
+		if (
+			message != null &&
+			typeof message === "object" &&
+			!React.isValidElement(message)
+		) {
+			Object.keys(message).forEach(k => {
+				TranslationComponent[k] = makeTranslationComponent(
+					joinPaths(messagePath, k)
+				);
+			});
+		}
+
+		return TranslationComponent;
+	}
+
+	return makeTranslationComponent("");
 }
 
 export default makeTranslationComponents;
