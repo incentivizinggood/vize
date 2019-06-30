1c64b8859d2dc44358dd8c6b7c49c052e664a82b
joshuatshaffer <joshuatshaffer@gmail.com>
2019-06-30 12:27:15 -0500

    Converted the workers' resources page to the new translation system.

--- meteor-app/imports/ui/translations/en.tsx
+++ meteor-app/imports/ui/translations/en.tsx
@@ -43,4 +43,32 @@ export default {
 		help: "Help",
 		allRightsReserved: "All rights reserved",
 	},
+	resourcesWorkers: {
+		readMore: "Read More →",
+		here: "here",
+		publishArticleMessage:
+			"Would you like to publish an article on our site? Email us your article to ",
+		trainingProgramsTitle: "Training Programs at UABC",
+		trainingProgramsDesc:
+			"Going back to school and getting a degree can be an enormous opportunity to increase your skill set as well as your wages, but many people don’t have the time or the money to finish an entire program.",
+		trainingProgramsArticle:
+			"Going back to school and getting a degree can be an enormous opportunity to increase your skill set as well as your wages, but many people don’t have the time or the money to finish an entire program. A degree can take years to complete and cost thousands of dollars. If you are working full time this can seem nearly impossible. There is another option, short-term training programs. These courses can give you a experience in accounting, management, writing, or much more. They generally only last about 1 month and are a fraction of the cost of a full degree. If you are interested in finding a course that fits your job and gives you the opportunity to increase your wages, you can find UABC’s programs ",
+		laborLawsTitle: "Learning about Labor Laws",
+		laborLawsDesc:
+			"It can be very difficult to know if your employer has broken labor laws in Mexico. If you’ve been having issues with your employer, an organization in downtown Tijuana, Ollin Calli may be able to help.",
+		laborLawsArticle:
+			"It can be very difficult to know if your employer has broken labor laws in Mexico. The legal system is very complicated especially if you’ve never been trained, but Mexico’s labor laws cover many more abuses than you would expect. If you’ve been having issues with your employer, an organization in downtown Tijuana, Ollin Calli may be able to help. They are a nonprofit that helps employees that have been taken advantage of by their employer. They can help with certain legal issues, provide you with more information about your rights, or give you a variety of options. If you want to reach them you can find their website ",
+		educationDistanceTitle: "Education at a Distance",
+		educationDistanceDesc:
+			"Increase your level of education with 'Educacion a Distancia', a course designed by Tecnologico Nacional de Mexico for people that work full-time but want to increase their skills to find a new and better paying job.",
+		educationDistanceArticlePt1:
+			"Making time to increase your level of education while working a full-time job can be very difficult. You may have to work 5 - 6 days a week with long hours, so going to a normal class during the week is impossible. The key is to find an education that allows you to be flexible with your schedule, and doesn’t require going to class every day. This is why the Tecnologico Nacional de Mexico created the Educacion a Distancia, a course designed for people that work full-time but want to increase their skills to find a new and better paying job. The courses are taught only on Saturdays for 4 - 6 hours and the rest of the class can be done entirely online with your phone or a computer. Right now there are two majors provided, Industrial Engineering and Logistics Engineering. If you’re interested you can check out their website ",
+		educationDistanceArticlePt2:
+			", or contact the Department of Academic Development at (664) 607-8410 ext 127 or email at ",
+		becomeBilingualTitle: "Become Bilingual!",
+		becomeBilingualDesc:
+			"If you can speak English you can often negotiate a much higher wage with your employer or find a higher paying job. We’ve listed the top five bilingual classes in Tijuana below with their contact information. Feel free to check them out!",
+		becomeBilingualArticle:
+			"Tijuana is the city of maquiladoras. Companies from all over the world come here to manufacture their products including Japan, Korea, and of course the United States. The one thing all of these companies have in common, is that their staff often speaks English. If you can speak English you can often negotiate a much higher wage with your employer or find a higher paying job. We’ve listed the top five bilingual classes in Tijuana below with their contact information. Feel free to check them out!",
+	},
 };
