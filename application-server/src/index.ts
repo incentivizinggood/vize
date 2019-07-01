3b88fe40a97778d00a5ada82d1b78c94cfac2339
joshuatshaffer <joshuatshaffer@gmail.com>
2019-07-01 13:38:15 -0500

    Got a hello world working with the existing PostgreSQL database.

--- application-server/src/index.ts
+++ application-server/src/index.ts
@@ -1,11 +1,18 @@
 import * as express from "express";
 import { ApolloServer } from "apollo-server-express";
+import { Pool } from "pg";
 
 import * as typeDefs from "./schema.graphql";
+
+const pool = new Pool();
 
 const resolvers = {
 	Query: {
 		hello: () => "Hello world!",
+		now: () =>
+			pool
+				.query("SELECT NOW() as now")
+				.then(res => res.rows[0].now.toISOString()),
 	},
 };
 
