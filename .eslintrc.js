0a0a418ef030aaea3c8d70c0466d16ea8fd4e878
joshuatshaffer <joshuatshaffer@gmail.com>
2019-07-06 09:52:46 -0500

    Got jsx-a11y working again.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
 	parser: "@typescript-eslint/parser",
-	plugins: ["react", "@typescript-eslint", "prettier"],
+	plugins: ["react", "jsx-a11y", "@typescript-eslint", "prettier"],
 	extends: [
 		"eslint:recommended",
 		"plugin:react/recommended",
+		"plugin:jsx-a11y/recommended",
 		"plugin:@typescript-eslint/recommended",
 		"prettier/@typescript-eslint",
 		"plugin:prettier/recommended",
@@ -12,16 +13,8 @@ module.exports = {
 		// Simple functions are better than classes. We should always use them
 		// if we are not using the class's lifecycle methods.
 		"react/prefer-stateless-function": 1,
-	},
-	/*
-		"jsx-a11y/anchor-is-valid": [
-			"error",
-			{
-				components: ["Link"],
-				specialLink: ["to"],
-			},
-		],
-		"jsx-a11y/label-has-for": 0,
+		// It is quite hard to make unique ids in React, so instead we put the
+		// control inside of its label to connect them.
 		"jsx-a11y/label-has-associated-control": [
 			"error",
 			{
@@ -31,5 +24,4 @@ module.exports = {
 			},
 		],
 	},
-	*/
 };
