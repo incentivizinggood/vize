c5d7785ac014e6b187c507e89999a182de44a792
joshuatshaffer <joshuatshaffer@gmail.com>
2019-07-06 09:38:16 -0500

    Got eslint to work with react.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
+	plugins: ["react", "@typescript-eslint", "prettier"],
 	extends: [
-		"@meteorjs/eslint-config-meteor",
-		"prettier",
-		"prettier/react",
+		"eslint:recommended",
+		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended",
+		"prettier/@typescript-eslint",
+		"plugin:prettier/recommended",
 	],
 	rules: {
-		"react/prop-types": 0,
+		// Simple functions are better than classes. We should always use them
+		// if we are not using the class's lifecycle methods.
+		"react/prefer-stateless-function": 1,
+	},
+	/*
 		"jsx-a11y/anchor-is-valid": [
 			"error",
 			{
@@ -26,4 +31,5 @@ module.exports = {
 			},
 		],
 	},
+	*/
 };
