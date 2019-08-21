cb618bf1b278f351a9bfb3f4185568ba27cc7896
joshuatshaffer <joshuatshaffer@gmail.com>
2019-08-21 13:53:03 -0500

    Made create salary mutation.

---
+++ meteor-app/imports/lib/inputs/salary.ts
@@ -0,0 +1,31 @@
+import * as yup from "yup";
+
+import LocationInput from "./location";
+
+type CreateSalaryInput = {
+	companyName: string;
+	location: LocationInput;
+	jobTitle: string;
+	incomeType: string;
+	incomeAmount: number;
+	gender?: string;
+};
+
+namespace CreateSalaryInput {
+	export const schema = yup.object({
+		companyName: yup.string().required(),
+		location: LocationInput.schema,
+		jobTitle: yup.string().required(),
+		incomeType: yup
+			.string()
+			.oneOf(["Yearly Salary", "Monthly Salary", "Hourly Wage"])
+			.required(),
+		incomeAmount: yup
+			.number()
+			.min(0)
+			.required(),
+		gender: yup.string().oneOf(["Male", "Female"]),
+	});
+}
+
+export default CreateSalaryInput;
