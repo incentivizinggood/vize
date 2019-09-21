6a27f2b1c9263e3ef1f2573c5a9182ea3d51023a
Julian Alvarez <julieanjear10@gmail.com>
2019-09-21 10:26:15 -0500

    Changed language to include Tijuana

--- meteor-app/imports/ui/translations/en.tsx
+++ meteor-app/imports/ui/translations/en.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default {
 	homePage: {
-		mainBanner: "Find a Great Job Near You",
+		mainBanner: "Find a Great Job Near You in Tijuana",
 		searchButton: "SEARCH",
 		placeholder: "Search for a Company...",
 		findjob_title: "Know The Job Before You Get It",
@@ -10,7 +10,7 @@ export default {
 		rewardText:
 			"Earn $100 pesos by sharing your work experience anonymously",
 		rewardText2:
-			"Your voice will help other workers find a great job at a factory that respects them",
+			"Your voice will help other workers find a great job, in Tijuana, at a factory that respects them",
 		findjob_text:
 			"Don't waste your time going from factory to factory, search job posts and read reviews from other employees to find the best job for you",
 		jobsButton: "Jobs",
@@ -133,7 +133,7 @@ export default {
 	createReview: {
 		formTitle: "Write a Review",
 		formSubTitle1:
-			"Your anonymous review will help other workers find a great job at a factory that respects them",
+			"Your anonymous review will help other workers find a great job, in Tijuana, at a factory that respects them.",
 		formSubTitle2: "Claim your $100 pesos with Swap or Paypal.",
 		fields: {
 			companyName: {
