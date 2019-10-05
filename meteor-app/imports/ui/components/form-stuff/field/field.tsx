0fb01c0c21da97f7bde5955d90e179d76246b77f
Julian <julianjear10@gmail.com>
2019-10-05 10:48:28 -0500

    Added spacing for all form fields so that error messages do not overlap with other fields

--- meteor-app/imports/ui/components/form-stuff/field/field.tsx
+++ meteor-app/imports/ui/components/form-stuff/field/field.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import * as Formik from "formik";
 import { TextField } from "formik-material-ui";
+import styled from "styled-components";
+
 
 import RatingField from "./rating-field";
 
@@ -19,12 +21,17 @@ const FieldInner: React.ComponentType<any> = ({ type, ...restProps }) => {
 	);
 };
 
-const Field: React.ComponentType<any> = ({ t: T, ...restProps }) => {
+const FieldComponent: React.ComponentType<any> = ({ t: T, ...restProps }) => {
 	if (T !== undefined) {
 		return <T renderer={t => <FieldInner {...restProps} {...t} />} />;
 	}
 
 	return FieldInner(restProps);
 };
+
+// Added this margin so that error messages do not overlap with other fields
+const Field = styled(FieldComponent)`
+	margin-bottom: 7px !important;
+`;
 
 export default Field;
