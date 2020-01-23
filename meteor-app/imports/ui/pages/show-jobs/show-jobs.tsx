601730429245900ba86bd681fd5cc138bc1d4234
Julian Alvarez <julieanjear10@gmail.com>
2020-01-22 22:57:54 -0600

    Refactored jobs page into a styled component and added some spacing on the sides

--- meteor-app/imports/ui/pages/show-jobs/show-jobs.tsx
+++ meteor-app/imports/ui/pages/show-jobs/show-jobs.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Query } from "react-apollo";
+import styled from "styled-components";
 
 import ShowJobComponent from "imports/ui/pages/show-jobs/show-job-component";
 import PageWrapper from "imports/ui/components/page-wrapper";
 import { translations } from "imports/ui/translations";
+import { forSize } from "imports/ui/responsive.js";
 
 import ShowJobsQuery from "./show-jobs.graphql";
 
 const T = translations.legacyTranslationsNeedsRefactor;
 
+const PageStyling = styled.div`
+	padding-top: 135px;
+	padding-bottom: 30px;
+	padding-right: 50px;
+	padding-left: 50px;
+
+	${forSize.tabletAndDown} {
+		padding-right: 10px;
+		padding-left: 10px;
+	}
+`;
+//background: #d8d8d8;
 const ShowJobs = () => (
 	<Query query={ShowJobsQuery}>
 		{({ loading, error, data }) => {
@@ -40,26 +54,15 @@ const ShowJobs = () => (
 			}
 
 			return (
-				<PageWrapper>
-					<div className="container-fluid  search_companies">
-						<div className="row all_boxcolor1 select_box1">
-							<div
-								id="companies_header1"
-								className="callbacks_container"
-							>
-								<ul id="slider3">
-									<li>
-										<h2>
-											{data.searchJobAds.nodes.length}{" "}
-											<T.jobsearch.jobsAvailable />
-										</h2>
-										{message}
-										{RenderedItems}
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
+				<PageWrapper title="Trabajos - Vize">
+					<PageStyling>
+						<h2>
+							{data.searchJobAds.nodes.length}{" "}
+							<T.jobsearch.jobsAvailable />
+						</h2>
+						{message}
+						{RenderedItems}
+					</PageStyling>
 				</PageWrapper>
 			);
 		}}
