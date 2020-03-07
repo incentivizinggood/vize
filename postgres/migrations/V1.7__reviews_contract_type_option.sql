100b2648cca49873a839ed7642eaae04b09a13b4
Julian Alvarez <julianjear10@gmail.com>
2020-03-06 20:45:57 -0600

    Set default value for contractType

--- postgres/migrations/V1.7__reviews_contract_type_option.sql
+++ postgres/migrations/V1.7__reviews_contract_type_option.sql
@@ -1,3 +1,4 @@
 ALTER TABLE reviews
-	ADD COLUMN contractType varchar(20)	NOT NULL 
-	CHECK (contractType='FULL_TIME' OR contractType='PART_TIME' OR contractType='INTERNSHIP' OR contractType='TEMPORARY' OR contractType='CONTRACTOR');
+	ADD COLUMN contractType text NOT NULL
+	CHECK (contractType='FULL_TIME' OR contractType='PART_TIME' OR contractType='INTERNSHIP' OR contractType='TEMPORARY' OR contractType='CONTRACTOR')
+	SET DEFAULT 'FULL_TIME';
