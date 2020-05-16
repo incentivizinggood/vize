73a892ae3a054f7ff541d225f8caf50a0e4c4faf
joshuatshaffer <joshuatshaffer@gmail.com>
2020-05-16 17:13:26 -0500

    Wrote call to create review. Need to pass session cookies around manualy.

--- api-test/index.ts
+++ api-test/index.ts
@@ -1,6 +1,6 @@
 import faker from "faker";
 import fetch from "node-fetch";
-import { execute, makePromise } from "apollo-link";
+import { execute, makePromise, GraphQLRequest } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
 
@@ -12,18 +12,99 @@ const createdData: any = {
 	users: [],
 };
 
-const operation = {
-	query: gql`
-		query {
-			say
+const CREATE_REVIEW = gql`
+	mutation createReview($reviewInput: CreateReviewInput!) {
+		createReview(input: $reviewInput) {
+			review {
+				id
+				company {
+					name
+				}
+				title
+				jobTitle
+				location {
+					address
+					city
+					industrialHub
+				}
+				numberOfMonthsWorked
+				contractType
+				employmentStatus
+				pros
+				cons
+				wouldRecommendToOtherJobSeekers
+				additionalComments
+				created
+				upvotes
+				downvotes
+				currentUserVote {
+					isUpvote
+				}
+			}
 		}
-	`,
-};
+	}
+`;
 
-// For single execution operations, a Promise can be used
-makePromise(execute(link, operation))
-	.then(data => console.log(`received data ${JSON.stringify(data, null, 2)}`))
-	.catch(error => console.log(`received error ${error}`));
+async function writeReview() {
+	const operation: GraphQLRequest = {
+		query: CREATE_REVIEW,
+		variables: {
+			reviewInput: {
+				companyName: faker.company.companyName(),
+				reviewTitle: faker.lorem.words(3),
+				location: {
+					city: faker.address.city(),
+					address: faker.address.streetAddress(),
+				},
+				jobTitle: faker.name.jobTitle(),
+				numberOfMonthsWorked: faker.random.number({ min: 1, max: 24 }),
+				contractType: faker.random.arrayElement([
+					"FULL_TIME",
+					"PART_TIME",
+					"INTERNSHIP",
+					"TEMPORARY",
+					"CONTRACTOR",
+				]),
+				employmentStatus: faker.random.arrayElement([
+					"FORMER",
+					"CURRENT",
+				]),
+				pros: faker.lorem.words(5),
+				cons: faker.lorem.words(5),
+				wouldRecommendToOtherJobSeekers: faker.random.boolean(),
+				healthAndSafety: faker.random.number({
+					min: 0,
+					max: 5,
+					precision: 1,
+				}),
+				managerRelationship: faker.random.number({
+					min: 0,
+					max: 5,
+					precision: 1,
+				}),
+				workEnvironment: faker.random.number({
+					min: 0,
+					max: 5,
+					precision: 1,
+				}),
+				benefits: faker.random.number({ min: 0, max: 5, precision: 1 }),
+				overallSatisfaction: faker.random.number({
+					min: 0,
+					max: 5,
+					precision: 1,
+				}),
+				additionalComments: faker.lorem.paragraph(),
+			},
+		},
+	};
+
+	// For single execution operations, a Promise can be used
+	makePromise(execute(link, operation))
+		.then(data =>
+			console.log(`received data ${JSON.stringify(data, null, 2)}`)
+		)
+		.catch(error => console.log(`received error ${error}`));
+}
 
 async function registerUser() {
 	const body = {
@@ -43,15 +124,16 @@ async function registerUser() {
 }
 
 async function main() {
-	const waitingon = [];
+	/*const waitingon = [];
 
 	for (let i = 0; i < 20; ++i) {
 		waitingon.push(registerUser());
 	}
 
-	await Promise.all(waitingon);
-
+	await Promise.all(waitingon);*/
+	await registerUser();
+	await writeReview();
 	console.log(createdData);
 }
 
-//main();
+main();
