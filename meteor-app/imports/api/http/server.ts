0396f21c58400df25d1f3292577023b9ba1a3315
joshuatshaffer <joshuatshaffer@gmail.com>
2020-05-21 12:24:21 -0500

    Improved session security.

--- meteor-app/imports/api/http/server.ts
+++ meteor-app/imports/api/http/server.ts
@@ -15,12 +15,27 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Warn if the SESSION_SECRET is not given. This is needed to sign the session
+// cookies and prevent spoofing.
+if (!process.env.SESSION_SECRET) {
+	console.error(
+		"The environment variable SESSION_SECRET is not set.",
+		"The default value will be used. This is insecure."
+	);
+}
+
 app.use(
 	expressSession({
 		store: new (connectPgSimple(expressSession))({ pool }),
-		secret: "keyboard cat",
+		secret: process.env.SESSION_SECRET || "keyboard cat",
 		resave: false,
 		saveUninitialized: false,
+		cookie: {
+			// Only send session cookies over secured HTTPS.
+			secure: true,
+			// Do not allow client side scripts to access the session cookie.
+			httpOnly: true,
+		},
 	})
 );
 
