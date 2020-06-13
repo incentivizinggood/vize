d183830300cc585c56405f9ea8d556cdee2f2e43
Julian <julianjear10@gmail.com>
2020-06-13 13:49:27 -0500

    Fixed naming in migration

--- postgres/migrations/V1.15__article_likes.sql
+++ postgres/migrations/V1.15__article_likes.sql
@@ -1,10 +1,10 @@
--- Resource Likes Table
--- If a user has liked the resource, then the entry will be created in the Table
--- If the user unlikes the resource, then the entry will be removed from the Table
+-- Article Likes Table
+-- If a user has liked the article, then the entry will be created in the Table
+-- If the user unlikes the article, then the entry will be removed from the Table
 DROP TABLE IF EXISTS article_likes CASCADE;
 CREATE TABLE article_likes (
 	user_id int NOT NULL,
-	resource_slug text NOT NULL,
+	article_slug text NOT NULL,
 	date_added date DEFAULT now(),
-	PRIMARY KEY (user_id, resource_slug)
+	PRIMARY KEY (user_id, article_slug)
 );
