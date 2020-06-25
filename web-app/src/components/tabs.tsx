c6aad26ca6c1f926aa2e81c3ee233f77e5c0854c
joshuatshaffer <joshuatshaffer@gmail.com>
2020-06-25 12:49:35 -0500

    Got the tab component looking decent.

--- web-app/src/components/tabs.tsx
+++ web-app/src/components/tabs.tsx
@@ -1,4 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const TabButton = styled.button`
+	background: black;
+	color: white;
+	flex-grow: 1;
+	font-size: x-large;
+	min-height: 2em;
+	&[aria-selected="true"] {
+		background: #439bd5;
+	}
+`;
+
+const TabHeader = styled.div`
+	display: flex;
+`;
 
 interface TabsProps {
 	tabs: {
@@ -12,15 +28,19 @@ export default function Tabs(props: TabsProps) {
 
 	return (
 		<div>
-			<div>
+			<TabHeader role="tablist">
 				{props.tabs.map((tab, index) => (
-					<button onClick={() => setTabIndex(index)}>
+					<TabButton
+						role="tab"
+						aria-selected={tabIndex === index}
+						onClick={() => setTabIndex(index)}
+					>
 						{tab.label}
-					</button>
+					</TabButton>
 				))}
-			</div>
+			</TabHeader>
 
-			<div>{props.tabs[tabIndex].content}</div>
+			<div role="tabpanel">{props.tabs[tabIndex].content}</div>
 		</div>
 	);
 }
