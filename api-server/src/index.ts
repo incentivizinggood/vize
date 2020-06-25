1763f9cb52eeed3d983d01ac91b3cbdff58ded67
joshuatshaffer <joshuatshaffer@gmail.com>
2020-06-25 18:52:47 -0500

    Added Helmet for security.

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 
 import { router as apiRouter } from "src/routes/api";
 import { router as webAppRouter } from "src/routes/web-app";
@@ -6,6 +7,8 @@ import { router as webAppRouter } from "src/routes/web-app";
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+app.use(helmet());
 
 app.set("trust proxy", 1);
 
