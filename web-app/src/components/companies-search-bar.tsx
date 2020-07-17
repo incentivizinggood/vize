0f107f41bbab7fc49a2df805fda7b68bc87c0b99
joshuatshaffer <joshuatshaffer@gmail.com>
2020-07-17 12:03:56 -0500

    Converted the companies search bar to use react-hook-form.

--- web-app/src/components/companies-search-bar.tsx
+++ web-app/src/components/companies-search-bar.tsx
@@ -1,77 +1,50 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { withRouter, RouteComponentProps } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 import { translations as T } from "src/translations";
 
-type CompaniesSearchBarProps = RouteComponentProps;
-
-interface CompaniesSearchBarState {
+type Inputs = {
 	search: string;
+};
+
+export default function CompaniesSearchBar() {
+	const location = useLocation();
+	const history = useHistory();
+
+	// The form data is saved in url search params.
+	// We load that data into the form so that the user can edit it
+	// and write it back to the url.
+	const params = new URLSearchParams(location.search);
+
+	const { register, handleSubmit } = useForm<Inputs>({
+		defaultValues: { search: params.get("search") || "" },
+	});
+
+	const onSubmit: SubmitHandler<Inputs> = data => {
+		console.log(data);
+		history.push(`/companies/?search=${data.search}`);
+	};
+
+	return (
+		<form className="form-search_header" onSubmit={handleSubmit(onSubmit)}>
+			<div className="search-bar-style">
+				<T.companiesSearchBar
+					renderer={({ placeholder }) => (
+						<input
+							name="search"
+							className="companies-search-input"
+							placeholder={placeholder}
+							ref={register}
+						/>
+					)}
+				/>
+				<button type="submit" className="search-icon">
+					<FontAwesomeIcon icon={faSearch} />
+				</button>
+			</div>
+		</form>
+	);
 }
-
-class CompaniesSearchBar extends React.Component<
-	CompaniesSearchBarProps,
-	CompaniesSearchBarState
-> {
-	constructor(props: Readonly<CompaniesSearchBarProps>) {
-		super(props);
-
-		// Purpose of getting params is so that the search text field doesn't reset to being empty after a search
-		const params = new URLSearchParams(location.search);
-		let searchParams = params.get("search");
-		if (searchParams === null) {
-			searchParams = "";
-		}
-
-		this.state = { search: searchParams };
-
-		// These bindings are necessary to make `this` work in callbacks.
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-		const { target } = event;
-		const value =
-			target.type === "checkbox" ? target.checked : target.value;
-		const { name } = target;
-
-		//@ts-ignore
-		this.setState({
-			[name]: value,
-		});
-	}
-
-	handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		this.props.history.push(`/companies/?search=${this.state.search}`);
-	}
-
-	render() {
-		return (
-			<form className="form-search_header" onSubmit={this.handleSubmit}>
-				<div className="search-bar-style">
-					<T.companiesSearchBar
-						renderer={({ placeholder }) => (
-							<input
-								type="text"
-								className="companies-search-input"
-								name="search"
-								placeholder={placeholder}
-								value={this.state.search}
-								onChange={this.handleInputChange}
-							/>
-						)}
-					/>
-					<button type="submit" className="search-icon">
-						<FontAwesomeIcon icon={faSearch} />
-					</button>
-				</div>
-			</form>
-		);
-	}
-}
-
-export default withRouter(CompaniesSearchBar);
