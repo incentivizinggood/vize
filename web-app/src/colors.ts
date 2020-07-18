9824e45d0c51063a3075e5969554f33863cfe700
joshuatshaffer <joshuatshaffer@gmail.com>
2020-07-18 12:58:41 -0500

    Merged old color variables into new colors.

--- web-app/src/colors.ts
+++ web-app/src/colors.ts
@@ -28,6 +28,20 @@ const colors = {
 	onBackgroundWeak: "lightgrey",
 	onSurfaceWeak: "lightgrey",
 	onSurfaceAccentWeak: "#cbcbcb",
+
+	/** The legacy variables used in styles. These are the same as in
+	 * src/sass/base/_variables.scss. We repeat them here so that they can be
+	 * used in newly converted styled components.
+	 */
+	old: {
+		concrete: "#f2f2f2",
+		seashell: "#f1f1f1",
+		outerSpace: "#343a40",
+		vizeBlue: "#439bd5",
+		secondaryColorRed: "#e35",
+		gallery: "#f0f0f0",
+		pictonBlue: "#58ace4",
+	},
 };
 
 export default colors;
