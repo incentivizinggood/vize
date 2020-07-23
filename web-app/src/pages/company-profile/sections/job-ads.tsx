f2499ac535563facfabc3ad1ff832793cbe0c296
joshuatshaffer <joshuatshaffer@gmail.com>
2020-07-23 10:13:35 -0500

    Made jobs section use the job article component.

--- web-app/src/pages/company-profile/sections/job-ads.tsx
+++ web-app/src/pages/company-profile/sections/job-ads.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faMapMarker,
-	faMoneyBillAlt,
-	faCalendar,
-} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+
+import { CompanyProfileJobsSectionFragment } from "generated/graphql-operations";
+import { translations } from "src/translations";
+
 import {
 	SectionContainer,
 	SectionHeaderContainer,
 	SectionHeaderTitle,
 	SeeMoreFooter,
 } from "../components";
-
-import { processLocation } from "src/misc";
-import { urlGenerators } from "src/pages";
-
-import { CompanyProfileJobsSectionFragment } from "generated/graphql-operations";
-import { translations } from "src/translations";
+import JobPosting from "../articles/job-ad";
 
 const T = translations.legacyTranslationsNeedsRefactor;
 
@@ -29,61 +21,11 @@ export default function JobsSection(props: JobsSectionProps): JSX.Element {
 	// FIRST JOB_AD CODE TO SHOW ON THE OVERVIEW TAB
 	let jobAdsToDisplay;
 	if (props.company.jobAds.length > 0) {
-		jobAdsToDisplay = (
-			<div>
-				<div>
-					<h4>
-						<strong>{props.company.jobAds[0].jobTitle}</strong>
-					</h4>
-				</div>
-				<div>
-					<div
-						className="add-buttons"
-						style={{ float: "right", marginTop: "0px" }}
-					>
-						<Link
-							to={urlGenerators.vizeApplyForJobUrl(
-								props.company.jobAds[0].id
-							)}
-							style={{ float: "right" }}
-							className="btn btn-primary"
-						>
-							{" "}
-							<T.overview_tab.apply_now />
-						</Link>
-					</div>
-					<p>
-						<FontAwesomeIcon icon={faMapMarker} />
-						&nbsp;&nbsp;&nbsp;
-						{processLocation(
-							JSON.stringify(props.company.jobAds[0].locations[0])
-						)}
-					</p>
-					<p>
-						<FontAwesomeIcon icon={faMoneyBillAlt} />
-						&nbsp;&nbsp;
-						{props.company.jobAds[0].pesosPerHour}
-						<T.overview_tab.hour />
-					</p>
-					<p>
-						<FontAwesomeIcon icon={faCalendar} />
-						&nbsp;&nbsp;
-						{props.company.jobAds[0].contractType}
-					</p>
-				</div>
-
-				<hr />
-				<h4 className="h4-font-sz-job">
-					<T.overview_tab.job_description />
-				</h4>
-				<div className="h4-font-sz">
-					<p>{props.company.jobAds[0].jobDescription}</p>
-				</div>
-			</div>
-		);
+		jobAdsToDisplay = <JobPosting jobAd={props.company.jobAds[0]} />;
 	} else {
 		jobAdsToDisplay = <T.overview_tab.no_jobs />;
 	}
+
 	return (
 		<SectionContainer>
 			<SectionHeaderContainer>
