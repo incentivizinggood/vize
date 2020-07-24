43ec16312102c1de6642e6c81a79a43510fb939f
joshuatshaffer <joshuatshaffer@gmail.com>
2020-07-24 09:14:46 -0500

    Made the 404 page look nicer.

--- web-app/src/pages/not-found.tsx
+++ web-app/src/pages/not-found.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 import PageWrapper from "src/components/page-wrapper";
+import Banner, { BannerTitle } from "src/components/banner";
+import { LinkButton } from "src/components/button";
 
-/* The "404" page.
- */
-export default function NotFoundPage() {
+/** The 404 page. */
+export default function NotFoundPage(): JSX.Element {
 	return (
 		<PageWrapper title="Not Found">
-			<h1>404</h1>
-			<h2>That page could not be found.</h2>
+			<Banner>
+				<BannerTitle>404 - Page not found.</BannerTitle>
+				<LinkButton to="/" $primary>
+					Go to home page
+				</LinkButton>
+			</Banner>
 		</PageWrapper>
 	);
 }
