64828474949ce6925384fbc5d219b409113d0e8f
joshuatshaffer <joshuatshaffer@gmail.com>
2020-08-08 12:27:15 -0500

    Added redirect to force the app to use https instead of http in production.

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -8,11 +8,20 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-app.use(helmet());
-
 // Trust the first proxy. This is needed so that secure cookies will be sent
 // even though this server does not have use TLS/SSL itself.
 app.set("trust proxy", 1);
+
+// Force the app to use https instead of http in production.
+if (process.env.NODE_ENV === "production") {
+	app.use(function(req, res) {
+		if (!req.secure) {
+			res.redirect(301, `https://${req.hostname}/${req.originalUrl}`);
+		}
+	});
+}
+
+app.use(helmet());
 
 app.use("/api", apiRouter);
 
