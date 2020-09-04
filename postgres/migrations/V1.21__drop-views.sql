ec5520e847911bf93298eca73ef7f76fbac88fff
joshuatshaffer <joshuatshaffer@gmail.com>
2020-09-04 12:01:15 -0500

    Reordered drop commands so that dependencies would not cause errors. Tested to work now.

--- postgres/migrations/V1.21__drop-views.sql
+++ postgres/migrations/V1.21__drop-views.sql
@@ -1,9 +1,10 @@
-drop view comment_vote_counts;
+-- Drop all views from the database.
 drop view comments_with_vote_counts;
+drop view comment_vote_counts;
 drop view company_review_statistics;
-drop view company_salary_income_amount;
 drop view company_salary_statistics;
+drop view company_salary_income_amount;
 drop view job_post_counts;
-drop view review_vote_counts;
 drop view reviews_with_vote_counts;
+drop view review_vote_counts;
 drop view salary_counts;
