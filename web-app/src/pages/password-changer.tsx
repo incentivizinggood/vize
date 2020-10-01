274b806f314cd59ba28f3e4bce5cf316083f5145
joshuatshaffer <joshuatshaffer@gmail.com>
2020-10-01 11:28:45 -0500

    Started upgrading password changer to use react-hook-form.

--- web-app/src/pages/password-changer.tsx
+++ web-app/src/pages/password-changer.tsx
@@ -1,48 +1,37 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { urlGenerators } from "src/pages/url-generators";
 
-import { withUser } from "src/hoc/user";
+import { withUser, useUser } from "src/hoc/user";
 import PageWrapper from "src/components/page-wrapper";
 import { translations } from "src/translations";
 import { changePassword } from "src/auth";
 
 const T = translations.legacyTranslationsNeedsRefactor.passwordChanger;
 
+interface Inputs {
+	oldPassword: string;
+	newPassword: string;
+	repeatNewPassword: string;
+}
+
 /* A form where users can change their passwords.
  */
-class PasswordChanger extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: this.props.user !== null ? null : "Not logged in",
-			success: false,
+export default function PasswordChanger() {
+	const user = useUser();
+
+	const { register, handleSubmit } = useForm<Inputs>({
+		defaultValues: {
 			oldPassword: "",
 			newPassword: "",
 			repeatNewPassword: "",
-		};
+		},
+	});
 
-		// These bindings are necessary to make `this` work in callbacks.
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleInputChange(event) {
-		const { target } = event;
-		const value =
-			target.type === "checkbox" ? target.checked : target.value;
-		const { name } = target;
-
-		this.setState({
-			[name]: value,
-		});
-	}
-
-	handleSubmit(event) {
-		event.preventDefault(); // Prevent the default behavior for this event.
-
+	const onSubmit: SubmitHandler<Inputs> = data => {
 		// Double check to avoid typos.
-		if (this.state.newPassword !== this.state.repeatNewPassword) {
+		if (data.newPassword !== data.repeatNewPassword) {
 			this.setState({
 				error: "New passwords do not match",
 				success: false,
@@ -51,8 +40,8 @@ class PasswordChanger extends React.Component {
 		}
 
 		changePassword({
-			oldPassword: this.state.oldPassword,
-			newPassword: this.state.newPassword,
+			oldPassword: data.oldPassword,
+			newPassword: data.newPassword,
 		})
 			.then(() =>
 				this.setState({
@@ -66,159 +55,117 @@ class PasswordChanger extends React.Component {
 					success: false,
 				})
 			);
-	}
+	};
 
-	renderContent() {
-		return (
-			<div
-				className="password-reset"
-				style={{ width: "80%", margin: "auto" }}
-			>
-				{this.state.error ? (
-					<div>
-						<T.error
-							renderer={t =>
-								// TODO: Some of the error messages,
-								// particularly ones from the server,
-								// will not be translated at the moment.
-								t[this.state.error] || this.state.error
-							}
-						/>
-					</div>
-				) : null}
-				<br />
-				<h3>
-					<strong>Change Password</strong>
-				</h3>
-				<br />
-				<form onSubmit={this.handleSubmit}>
-					<label
-						htmlFor="passwordChangeForm-oldPassword"
-						style={{ width: "100%" }}
+	return (
+		<PageWrapper title="Account Recovery">
+			<div className="container form-page">
+				<div className="row">
+					<div
+						className="back_top_hover"
+						style={{ backgroundColor: "#ffffff" }}
 					>
-						<T.oldPassword
-							renderer={t => (
-								<input
-									id="passwordChangeForm-oldPassword"
-									name="oldPassword"
-									type="password"
-									placeholder={t}
-									required
-									value={this.state.oldPassword}
-									onChange={this.handleInputChange}
-									style={{ width: "100%" }}
-								/>
-							)}
-						/>
-					</label>
-					<br />
-					<br />
-					<label
-						htmlFor="passwordChangeForm-newPassword"
-						style={{ width: "100%" }}
-					>
-						<T.newPassword
-							renderer={t => (
-								<input
-									id="passwordChangeForm-newPassword"
-									name="newPassword"
-									type="password"
-									placeholder={t}
-									required
-									value={this.state.newPassword}
-									onChange={this.handleInputChange}
-									style={{ width: "100%" }}
-								/>
-							)}
-						/>
-					</label>
-					<br />
-					<br />
-					<label
-						htmlFor="passwordChangeForm-repeatNewPassword"
-						style={{ width: "100%" }}
-					>
-						<T.newPassword
-							renderer={t => (
-								<input
-									id="passwordChangeForm-repeatNewPassword"
-									name="repeatNewPassword"
-									type="password"
-									placeholder={t}
-									required
-									value={this.state.repeatNewPassword}
-									onChange={this.handleInputChange}
-									style={{ width: "100%" }}
-								/>
-							)}
-						/>
-					</label>
-					<br />
-					<br />
-					<T.submit
-						renderer={t => (
-							<input
-								type="submit"
-								className="btn btn-primary"
-								value={t}
-							/>
-						)}
-					/>
-					<br />
-					<br />
-				</form>
-			</div>
-		);
-	}
+						<br />
 
-	render() {
-		let content = null;
-
-		if (this.state.success) {
-			content = (
-				<div
-					style={{ width: "80%", margin: "0 auto" }}
-					className="password-reset"
-				>
-					<T.success />
-					<br />
-					<br />
-				</div>
-			);
-		} else if (this.props.user) {
-			content = this.renderContent();
-		} else {
-			content = (
-				<div style={{ width: "80%", margin: "0 auto" }}>
-					<br />
-					You must be logged in to use this page. <br /> <br />
-					<Link
-						className="btn btn-primary"
-						to={urlGenerators.vizeLogin("worker")}
-					>
-						Log In
-					</Link>
-					<br />
-				</div>
-			);
-		}
-
-		return (
-			<PageWrapper title="Account Recovery">
-				<div className="container form-page">
-					<div className="row">
 						<div
-							className="back_top_hover"
-							style={{ backgroundColor: "#ffffff" }}
+							className="password-reset"
+							style={{ width: "80%", margin: "auto" }}
 						>
+							{this.state.error ? (
+								<div>
+									<T.error
+										renderer={t =>
+											// TODO: Some of the error messages,
+											// particularly ones from the server,
+											// will not be translated at the moment.
+											t[this.state.error] ||
+											this.state.error
+										}
+									/>
+								</div>
+							) : null}
 							<br />
-							{content}
+							<h3>
+								<strong>Change Password</strong>
+							</h3>
+							<br />
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<label
+									htmlFor="passwordChangeForm-oldPassword"
+									style={{ width: "100%" }}
+								>
+									<T.oldPassword
+										renderer={t => (
+											<input
+												id="passwordChangeForm-oldPassword"
+												name="oldPassword"
+												type="password"
+												placeholder={t}
+												required
+												ref={register}
+												style={{ width: "100%" }}
+											/>
+										)}
+									/>
+								</label>
+								<br />
+								<br />
+								<label
+									htmlFor="passwordChangeForm-newPassword"
+									style={{ width: "100%" }}
+								>
+									<T.newPassword
+										renderer={t => (
+											<input
+												id="passwordChangeForm-newPassword"
+												name="newPassword"
+												type="password"
+												placeholder={t}
+												required
+												ref={register}
+												style={{ width: "100%" }}
+											/>
+										)}
+									/>
+								</label>
+								<br />
+								<br />
+								<label
+									htmlFor="passwordChangeForm-repeatNewPassword"
+									style={{ width: "100%" }}
+								>
+									<T.newPassword
+										renderer={t => (
+											<input
+												id="passwordChangeForm-repeatNewPassword"
+												name="repeatNewPassword"
+												type="password"
+												placeholder={t}
+												required
+												ref={register}
+												style={{ width: "100%" }}
+											/>
+										)}
+									/>
+								</label>
+								<br />
+								<br />
+								<T.submit
+									renderer={t => (
+										<input
+											type="submit"
+											className="btn btn-primary"
+											value={t}
+										/>
+									)}
+								/>
+								<br />
+								<br />
+							</form>
 						</div>
 					</div>
 				</div>
-			</PageWrapper>
-		);
-	}
+			</div>
+		</PageWrapper>
+	);
 }
-
-export default withUser(PasswordChanger);
