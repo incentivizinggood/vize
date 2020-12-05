a1f6d38d7860dfdb8ccefb70910983aa4a30166d
joshuatshaffer <joshua@joshuatshaffer.com>
2020-12-05 13:38:07 -0600

    Added translations to the 404 page.

--- web-app/src/pages/not-found.tsx
+++ web-app/src/pages/not-found.tsx
@@ -3,15 +3,29 @@ import React from "react";
 import PageWrapper from "src/components/page-wrapper";
 import Banner, { BannerTitle } from "src/components/banner";
 import { LinkButton } from "src/components/button";
+import { makeTranslationHook } from "src/translations/translations";
+
+const useTranslations = makeTranslationHook({
+	en: {
+		title: "404 - Page not found",
+		goHome: "Go to home page",
+	},
+	es: {
+		title: "404 - Página no encontrada",
+		goHome: "Ir a la página de inicio",
+	},
+});
 
 /** The 404 page. */
 export default function NotFoundPage(): JSX.Element {
+	const t = useTranslations();
+
 	return (
-		<PageWrapper title="Not Found">
+		<PageWrapper title={t.title}>
 			<Banner>
-				<BannerTitle>404 - Page not found.</BannerTitle>
+				<BannerTitle>{t.title}</BannerTitle>
 				<LinkButton to="/" $primary>
-					Go to home page
+					{t.goHome}
 				</LinkButton>
 			</Banner>
 		</PageWrapper>
