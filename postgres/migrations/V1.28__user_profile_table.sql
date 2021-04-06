2616a7005473dc2cafde9f9c3eba9a38124ce8d4
julianjear <julianjear10@gmail.com>
2021-04-05 21:20:54 -0500

    Created user profiles table

---
+++ postgres/migrations/V1.28__user_profile_table.sql
@@ -0,0 +1,30 @@
+-- User Profiles Table
+DROP TABLE IF EXISTS user_profiles CASCADE;
+CREATE TABLE user_profiles (
+	userid serial PRIMARY KEY,
+	full_name VARCHAR(40) NOT NULL,
+	phone_number VARCHAR(16) NOT NULL,
+	location_city text NOT NULL,
+	location_neighborhood text,
+	location_address text,
+    work_experiences jsonb,
+	skills text[],
+	certificates_and_licences text,
+	education_level text
+        NOT NULL
+        CHECK (education_level IN ('SOME_HIGH_SCHOOL', 'HIGH_SCHOOL', 'SOME_COLLEGE', 'COLLEGE_DEGREE')),
+    work_availability text[]
+        NOT NULL
+        CHECK (work_availability IN ('MORNING_SHIFT', 'AFTERNOON_SHIFT', 'NIGHT_SHIFT')),
+    availability_comments text,
+    long_term_professional_goal text,
+);
+
+COMMENT ON COLUMN user_profiles.long_term_professional_goal IS
+	'Where the worker wants to be professionally in 3-10 years';
+
+COMMENT ON COLUMN user_profiles.location_neighborhood IS
+	'The neighborhood or area of the city where the worker is located';
+
+COMMENT ON COLUMN user_profiles.education_level IS
+	'The highest level of education a worker has achieved';
