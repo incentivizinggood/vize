809cf9da19071157f6feee421b63adf361190214
Julian Alvarez <julianjear10@gmail.com>
2021-05-25 07:19:15 -0500

    Refactored My Account Page a bit

--- web-app/src/pages/my-account.tsx
+++ web-app/src/pages/my-account.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useUser } from "src/hoc/user";
 
 import { withUser } from "src/hoc/user";
 import PageWrapper from "src/components/page-wrapper";
 
+import LinkButton from "src/components/button/link-button";
+
+import Spinner from "src/components/Spinner";
 import * as urlGenerators from "src/pages/url-generators";
 import { translations } from "src/translations";
+import { useGetUserProfileDataQuery } from "generated/graphql-operations";
 
 const T = translations.myAccount;
 const TLoggedIn = translations.needToBeLoggedInToView;
@@ -13,10 +18,32 @@ const TLoggedIn = translations.needToBeLoggedInToView;
 /* The page where users can view their account details,
  * update their profiles, and change settings.
  */
-class MyAccountPage extends React.Component {
-	renderContent() {
-		return (
-			<div style={{ width: "80%", margin: "0 auto" }}>
+export default function MyAccountPage() {
+	const user = useUser();
+
+	// let { data: userProfileData, loading, error } = useGetUserProfileDataQuery({
+	// 	variables: { userId: user ? user.id : "0" },
+	// });
+
+	// if (loading) return <Spinner />;
+
+	// console.log('userProfile', userProfileData);
+	// console.log('userProfileload', loading);
+	// console.log('userProfileerror', error);
+	// let userProfile = null;
+
+	// // If user has a user profile, fill in the form fields with the user profile data
+	// if(userProfileData?.userProfile) {
+	// 	userProfile = formatUserProfileData(userProfileData.userProfile);
+	// 	profileFormTitle = (<T.formTitleEditProfile />);
+	// }
+
+	
+		let content = null;
+		if (user) {
+			console.log('user', user);
+			content = (
+				<div style={{ width: "80%", margin: "0 auto" }}>
 				<br />
 				<h2>
 					<strong><T.myAccount /></strong>
@@ -24,34 +51,28 @@ class MyAccountPage extends React.Component {
 				<hr />
 				<div>
 					<strong><T.username />: </strong>
-					{this.props.user.username}
+					{user.username}
 					<br />
 					<br />
-					<Link className="btn btn-info" to={`/${urlGenerators.queryRoutes.changePassword}`}>
+					<LinkButton to={`/${urlGenerators.queryRoutes.changePassword}`} $primary>
 						<T.changePassword />
-					</Link>
+					</LinkButton>
 					<br />
 					<br />
 				</div>
 			</div>
-		);
-	}
-
-	render() {
-		let content = null;
-		if (this.props.user) {
-			content = this.renderContent();
+			);
 		} else {
 			content = (
 				<div style={{ width: "80%", margin: "0 auto" }}>
 					<br />
 					<TLoggedIn.youNeedToBeLoggedInToView /> <br /> <br />
-					<Link
-						className="btn btn-primary"
+					<LinkButton
 						to={urlGenerators.vizeLogin("worker")}
+						$primary
 					>
 						<TLoggedIn.login />
-					</Link>
+					</LinkButton>
 
 					<br />
 					<br />
@@ -74,7 +95,5 @@ class MyAccountPage extends React.Component {
 				</div>
 			</PageWrapper>
 		);
-	}
+	
 }
-
-export default withUser(MyAccountPage);
