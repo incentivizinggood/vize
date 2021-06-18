de0f58a1c7c0056524fc9dbee8024bb917684267
Julian Alvarez <julianjear10@gmail.com>
2021-06-18 17:08:16 -0500

    Improved UI for Apply Now Button

---
+++ api-server/src/utils/translation-utils.ts
@@ -0,0 +1,27 @@
+export const monthTranslations = {
+	0: "enero",
+	1: "febrero",
+	2: "marzo",
+	3: "abril",
+	4: "mayo",
+	5: "junio",
+	6: "julio",
+	7: "agosto",
+	8: "septiembre",
+	9: "octubre",
+	10: "noviembre",
+	11: "diciembre",
+};
+
+export const workShiftTranlsations = {
+    "MORNING_SHIFT": "Turno Matutino",
+    "AFTERNOON_SHIFT": "Turno Vespertino",
+    "NIGHT_SHIFT": "Turno Nocturno",
+};
+
+export const educationTranslations = {
+    "SOME_HIGH_SCHOOL": "Preparatoria Trunca",
+    "HIGH_SCHOOL": "Preparatoria",
+    "SOME_COLLEGE": "Universidad Trunca",
+    "COLLEGE_DEGREE": "Título Universitario",
+};
