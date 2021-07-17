0063260c69c3294de6827f313db1570befb4a33f
Julian Alvarez <julianjear10@gmail.com>
2021-07-17 17:47:36 -0500

    Fixed a bug where viewing a single job post was not displaying the error correctly if the job id does not exist

--- web-app/src/pages/job-ad.tsx
+++ web-app/src/pages/job-ad.tsx
@@ -5,6 +5,7 @@ import PageWrapper from "src/components/page-wrapper";
 import JobPosting from "src/components/jobs/job-posting";
 import { useJobAdPageQuery } from "generated/graphql-operations";
 import Spinner from "src/components/Spinner";
+import { PanelContainer, Panel } from "src/components/panel";
 
 const Center = styled.div`
 	margin-left: auto;
@@ -18,23 +19,31 @@ interface JobAdPageProps {
 	jobAdId: string;
 }
 
+// This page is to view a single job post (which can currently only be done through a direct URL)
 export function JobAdPage({ jobAdId }: JobAdPageProps): JSX.Element {
-	// const { loading, data } = useJobAdPageQuery({
-	// 	variables: { jobAdId },
-	// });
+	const { loading, data } = useJobAdPageQuery({
+		variables: { jobAdId },
+	});
 
 	return (
 		<PageWrapper>
-			{/* {loading ? (
+			{loading ? (
 				<Spinner />
 			) : data && data.jobAd ? (
 				<Center>
-					
+					<JobPosting job={data.jobAd} isMinimizable={false} />
 				</Center>
 			) : (
-				"Could not find that job ad."
-			)} */}
-			hello
+				<PanelContainer>
+					<Panel roundedEdges>
+						{/* TODO: Add better error reporting */}
+						<p>
+							La oferta de empleo no se pudo encontrar. Es
+							probable que la empresa elimino la oferta.
+						</p>
+					</Panel>
+				</PanelContainer>
+			)}
 		</PageWrapper>
 	);
 }
