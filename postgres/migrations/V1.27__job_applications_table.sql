0d32296e44d95139dce3701c5269e11721cabcc1
joshuatshaffer <joshua@joshuatshaffer.com>
2021-07-31 12:43:37 -0500

    Moved column additions to new migration.

--- postgres/migrations/V1.27__job_applications_table.sql
+++ postgres/migrations/V1.27__job_applications_table.sql
@@ -5,28 +5,7 @@ CREATE TABLE job_applications (
 	full_name VARCHAR(40) NOT NULL,
     phone_number VARCHAR(15) NOT NULL,
 	email text NOT NULL,
-	location_city text NOT NULL,
-	location_neighborhood text,
-    work_experiences jsonb,
-	skills text[],
-	certificates_and_licences text[],
-	english_proficiency text
-        NOT NULL
-        CHECK (english_proficiency IN ('NATIVE_LANGUAGE', 'FLUENT', 'CONVERSATIONAL', 'BASIC', 'NO_PROFICIENCY')),
-	education_level text
-        NOT NULL
-        CHECK (education_level IN ('SOME_HIGH_SCHOOL', 'HIGH_SCHOOL', 'SOME_COLLEGE', 'COLLEGE_DEGREE')),
-    work_availability text[]
-        NOT NULL
-        CHECK (work_availability <@ ARRAY['MORNING_SHIFT', 'AFTERNOON_SHIFT', 'NIGHT_SHIFT']),
-    availability_comments text,
 	cover_letter text,
 	jobadid INTEGER,
 	companyid INTEGER
 );
-
-COMMENT ON COLUMN job_applications.location_neighborhood IS
-	'The neighborhood or area of the city where the worker is located';
-
-COMMENT ON COLUMN job_applications.education_level IS
-	'The highest level of education a worker has achieved';
--- postgres/migrations/V1.30__job_applications_table_add_timestamp.sql
+++ postgres/migrations/V1.30__job_applications_table_add_timestamp.sql
@@ -1,2 +1,41 @@
 ALTER TABLE job_applications 
 	ADD COLUMN date_added timestamp with time zone default now();
+
+ALTER TABLE job_applications 
+	ADD COLUMN location_city text NOT NULL;
+
+ALTER TABLE job_applications 
+	ADD COLUMN location_neighborhood text;
+
+COMMENT ON COLUMN job_applications.location_neighborhood IS
+	'The neighborhood or area of the city where the worker is located';
+
+ALTER TABLE job_applications 
+	ADD COLUMN work_experiences jsonb;
+
+ALTER TABLE job_applications 
+	ADD COLUMN skills text[];
+
+ALTER TABLE job_applications 
+	ADD COLUMN certificates_and_licences text[];
+
+ALTER TABLE job_applications 
+	ADD COLUMN english_proficiency text
+        NOT NULL
+        CHECK (english_proficiency IN ('NATIVE_LANGUAGE', 'FLUENT', 'CONVERSATIONAL', 'BASIC', 'NO_PROFICIENCY'));
+
+ALTER TABLE job_applications 
+	ADD COLUMN education_level text
+        NOT NULL
+        CHECK (education_level IN ('SOME_HIGH_SCHOOL', 'HIGH_SCHOOL', 'SOME_COLLEGE', 'COLLEGE_DEGREE'));
+
+COMMENT ON COLUMN job_applications.education_level IS
+	'The highest level of education a worker has achieved';
+
+ALTER TABLE job_applications 
+	ADD COLUMN work_availability text[]
+        NOT NULL
+        CHECK (work_availability <@ ARRAY['MORNING_SHIFT', 'AFTERNOON_SHIFT', 'NIGHT_SHIFT']);
+
+ALTER TABLE job_applications 
+	ADD COLUMN availability_comments text;
