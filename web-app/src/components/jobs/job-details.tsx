c6bd51d714852913b7aebf0f11fa6d57e9918fdc
julianjear <julianjear10@gmail.com>
2021-09-16 01:18:27 -0500

    fixed salary bar file name

--- web-app/src/components/jobs/job-details.tsx
+++ web-app/src/components/jobs/job-details.tsx
@@ -3,21 +3,21 @@ import { withStyles } from "@material-ui/core/styles";
 import styled from "styled-components";
 import { borderRadius } from "src/global-styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import SalaryBar from "./SalaryBar";
+import SalaryBar from "./salary-bar";
 
 const BorderLinearProgress = withStyles((theme) => ({
-    root: {
-        height: 35,
-        borderRadius: 16,
-    },
-    colorPrimary: {
-        backgroundColor:
-            theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
-    },
-    bar: {
-        borderRadius: 16,
-        backgroundColor: "#1a90ff",
-    },
+	root: {
+		height: 35,
+		borderRadius: 16,
+	},
+	colorPrimary: {
+		backgroundColor:
+			theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
+	},
+	bar: {
+		borderRadius: 16,
+		backgroundColor: "#1a90ff",
+	},
 }))(LinearProgress);
 const StyledJobDetails = styled.div`
 	padding: 15px;
@@ -47,25 +47,21 @@ const SalaryRangeValues = styled.span`
 	justify-content: space-between;
 `;
 interface JobDetailsProps {
-    position: string;
-    pay: number;
-    range: number[]
+	position: string;
+	pay: number;
+	range: number[];
 }
 export default function JobDetails(props: JobDetailsProps): JSX.Element {
-    const { position, pay, range } = props
-    return (
-        <StyledJobDetails >
-            <ReviewHeader>
-                <SalaryJobTitle>
-                    {position}
-                </SalaryJobTitle>
-            </ReviewHeader>
-            <AveeragePay>
-                <GreyText>Average</GreyText>{" "}
-                <span>: $ {pay} / week</span>
-            </AveeragePay>
-            <SalaryBar range={range}>
-            </SalaryBar>
-        </StyledJobDetails>
-    );
+	const { position, pay, range } = props;
+	return (
+		<StyledJobDetails>
+			<ReviewHeader>
+				<SalaryJobTitle>{position}</SalaryJobTitle>
+			</ReviewHeader>
+			<AveeragePay>
+				<GreyText>Average</GreyText> <span>: $ {pay} / week</span>
+			</AveeragePay>
+			<SalaryBar range={range}></SalaryBar>
+		</StyledJobDetails>
+	);
 }
